3b159d2389a058f1af759ec544896a969e6574a3
jaywcjlove <398188662@qq.com>
2017-05-17 15:24:36 +0800

    button接口设计

--- src/buttons/buttons.js
+++ src/buttons/buttons.js
@@ -1,19 +1,45 @@
 
 import React, { Component } from 'react';
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
 
 export default class Buttons extends Component {
+  static defaultProps = {
+    disabled: false,
+    active: false,
+    type: 'default',
+    size: 'normal',
+  };
+  static propTypes = {
+    disabled: PropTypes.bool,
+    active: PropTypes.bool,
+    size: PropTypes.string,
+    type: PropTypes.string,
+  }
   render() {
-    const {type, size, className, children, ...others } = this.props;
+    const {type, size, active, className, children, ...others } = this.props;
     const cls = classNames({
           'w-btn': true,
-          'w-btn-small': size === 'small',
-          'w-btn-primary': type === 'primary',
-          'w-btn-default': type === 'default',
-          'w-btn-warn': type === 'warn',
-          'w-btn-disabled': this.props.disabled,
+
+          'w-btn-extra-small': size === 'extra-small',  //（超小尺寸）Extra small button
+          'w-btn-small': size === 'small',              //（小按钮）Small button
+          'w-btn-default': size === 'default',          //（默认尺寸）Default button
+          'w-btn-large': size === 'large',              //（大按钮）Large button
+
+          'w-btn-default': type === 'default',  //  默认样式
+          'w-btn-primary': type === 'primary',  // （首选项）Primary
+          'w-btn-success': type === 'success',  // （首选项）Primary
+          'w-btn-info': type === 'info',        // （一般信息）Info
+          'w-btn-warn': type === 'warn',        // （警告）Warning
+          'w-btn-danger': type === 'danger',    // （危险）Danger
+          'w-btn-link': type === 'link',        // （链接）Link
+
+          'disabled': this.props.disabled,    // 禁用状态
+          'active': this.props.active,      // 激活状态
+
           [className]: className
       });
+    console.log("cls:::",cls)
     return (
       <button { ...others } className={ cls }>{ children }</button>
     );
