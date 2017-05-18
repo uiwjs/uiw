4c2f09afbde09d39af3d6576c7854f62d8311d5c
jaywcjlove <398188662@qq.com>
2017-05-19 00:35:14 +0800

    Update close icon.

--- src/svgs.js
+++ src/svgs.js
@@ -3,9 +3,9 @@ import React from 'react';
 
 
 const IconClose = (
-  <svg width="10" height="10" viewBox="0 0 32 32">
-    <g transform="scale(0.03125, 0.03125)">
-      <path d="M992 924.768 924.768 992 512 579.232 99.232 992 32 924.768 444.768 512 32 99.232 99.232 32 512 444.768 924.768 32 992 99.232 579.232 512Z" fill="#ffffff" />
+  <svg width="10px" height="10px" viewBox="0 0 10 10">
+    <g transform="translate(-318.000000, -20.000000)" fill="#000000">
+      <path d="M324,24 L324,20 L322,20 L322,24 L318,24 L318,26 L322,26 L322,30 L324,30 L324,26 L328,26 L328,24 L324,24 Z" id="close-icon" transform="translate(323.000000, 25.000000) rotate(-45.000000) translate(-323.000000, -25.000000) "></path>
     </g>
   </svg>
 )
