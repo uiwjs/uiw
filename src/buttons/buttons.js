570c402176174ac57b4716c4fe2d2b031a91bfd1
王楚江 <wcj@nihaosi.com>
2017-05-20 18:59:03 +0800

    修复button样式bug

--- src/buttons/buttons.js
+++ src/buttons/buttons.js
@@ -23,21 +23,19 @@ export default class Buttons extends Component {
   }
   render() {
     const { prefixCls, type, size, active, disabled, block, className, loading, children, ...others } = this.props;
+
+    let types = type;
+    switch(type){
+      case 'error': types='danger';break;
+      default:      types = type;  break;
+    }
     const cls = classNames(prefixCls,{
         [`${prefixCls}-extra-small`]: size === 'extra-small', //（超小尺寸）Extra small button
         [`${prefixCls}-small`]: size === 'small',             //（小按钮）Small button
         [`${prefixCls}-default`]: size === 'default',         //（默认尺寸）Default button
         [`${prefixCls}-large`]: size === 'large',             //（大按钮）Large button
-        
-        [`${prefixCls}-default`]: type === 'default',         //  默认样式
-        [`${prefixCls}-primary`]: type === 'primary',         // （首选项）Primary
-        [`${prefixCls}-success`]: type === 'success',         // （成功）Success
-        [`${prefixCls}-info`]: type === 'info',               // （一般信息）Info
-        [`${prefixCls}-warn`]: type === 'warn',               // （警告）Warning
-        [`${prefixCls}-danger`]: type === 'danger',           // （危险）Danger
-        [`${prefixCls}-danger`]: type === 'error',            // （危险）Danger
-
-        [`${prefixCls}-loading`]: loading,                    // （危险）Danger
+        [`${prefixCls}-${types}`]: /^(default|primary|success|info|warn|danger)?$/.test(type),
+        [`${prefixCls}-loading`]: loading,                    // 加载
 
         'disabled': disabled || loading,    // 禁用状态
         'active': active,        // 激活状态
@@ -45,6 +43,7 @@ export default class Buttons extends Component {
 
         [className]: className
       });
+    console.log("type::",children,'-2:',type,'-3:',cls)
     return (
       <button { ...others } disabled={disabled || loading} className={ cls }>{loading&&IconLoading}{ children }</button>
     );
