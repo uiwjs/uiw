b59e08726a47e7f4181134265de690ac64c71d32
王楚江 <wcj@nihaosi.com>
2017-05-22 23:03:57 +0800

    日历初步效果

--- src/calendar/heat-map.js
+++ src/calendar/heat-map.js
@@ -5,9 +5,37 @@ import "./style/index.less";
 
 export default class HeatMap extends Component {
   render() {
-    const { prefixCls } = this.props;
+    const { prefixCls, days, className} = this.props;
+    const cls = classNames(prefixCls,{
+      [className]: className
+    });
+
+    let width=14, height=14;
+
+    // 日历
+    var rectdays = [],col=16;
+    for (var i = 0; i < days; i++) {
+      let xl = 21 + parseInt(i/7) * col;
+      let yl = 17 + parseInt(i%7) * col;
+      rectdays.push(<rect key={i} fill="#EBEDF0" x={xl} y={yl} width={width} height={height}></rect>);
+    }
+
     return (
-      <div></div>
+      <svg className={ cls } width="900px" height="128px">
+        <g className={ `${prefixCls}-week` } transform="translate(0, 0)">
+          <rect x="0" y="17" width={width} height={height}></rect>
+          <rect x="0" y="33" width={width} height={height}></rect>
+          <rect x="0" y="49" width={width} height={height}></rect>
+          <rect x="0" y="65" width={width} height={height}></rect>
+          <rect x="0" y="81" width={width} height={height}></rect>
+          <rect x="0" y="97" width={width} height={height}></rect>
+          <rect x="0" y="113" width={width} height={height}></rect>
+        </g>
+        {rectdays}
+        <text>
+          <tspan x="21" y="17">se</tspan>
+        </text>
+      </svg>
     );
   }
 }
@@ -15,11 +43,13 @@ export default class HeatMap extends Component {
 HeatMap.propTypes = {
   weekLables:PropTypes.object,
   monthLables:PropTypes.array,
+  days:PropTypes.number,
   panelColors:PropTypes.object,
 }
 
 HeatMap.defaultProps = {
-  prefixCls: "w-heatmap", 
+  prefixCls: "w-heatmap",
+  days:365,
   // 默认选填选项  周标签显示
   weekLables: {0:'S', 1:'M', 2:'T', 3:'W', 4:'T', 5:'T', 6:'T', },
   // 默认选填选项  月份标签显示
