0853ba5e2c1829b811b244d52359312d79cf2c29
王楚江 <wcj@nihaosi.com>
2017-05-25 03:00:47 +0800

    Tables添加onCellClick事件

--- src/table/tbody.js
+++ src/table/tbody.js
@@ -9,12 +9,16 @@ export default class Tbody extends Component{
   getRenders(columns,headelm={}){
     let subitem = [];
     for(let i =0; i< columns.length;i++){
-      if(columns[i]
-        && (!columns[i].children || columns[i].children.length < 1) 
-        && columns[i].render
-        &&columns[i].key ){
-        headelm[columns[i].key] = columns[i].render;
+      if(columns[i] && columns[i].key ){
+        headelm[columns[i].key]={}
+        if((!columns[i].children || columns[i].children.length < 1) && columns[i].render){
+          headelm[columns[i].key]['render'] = columns[i].render;
+        }
+        if((!columns[i].children || columns[i].children.length < 1) && columns[i].onCellClick){
+          headelm[columns[i].key]['onCellClick'] = columns[i].onCellClick;
+        }
       }
+
       if(columns[i].children && columns[i].children.length){
         this.getRenders(columns[i].children,headelm);
       }
@@ -22,12 +26,16 @@ export default class Tbody extends Component{
     return headelm;
   }
   renderTbodyTd(item){
-    const {render,columns} = this.props;
-    var renders = this.getRenders(columns)
+    const {columns} = this.props;
+    var renders = this.getRenders(columns);
     let items = [],key=0;
     for(let a in item){
       ++key;
-      items.push(<td key={key}>{renders[a]?renders[a](item[a],item,key):item[a]}</td>);
+      let attri = {}
+      if(renders[a]&&renders[a].onCellClick){
+        attri.onClick = renders[a].onCellClick.bind(this,item[a])
+      }
+      items.push(<td key={key} {...attri}>{(renders[a]&&renders[a].render)?renders[a].render(item[a],item,key):item[a]}</td>);
     }
     return items;
   }
