16bf2f282c9e245e5b8812239cd352e1dfb4a458
王楚江 <wcj@nihaosi.com>
2017-05-30 15:23:26 +0800

    alerts 添加图标显示

--- src/alerts/alerts.js
+++ src/alerts/alerts.js
@@ -3,7 +3,7 @@ import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup'
 import classNames from 'classnames';
 import Transition from '../transition'
 import PropTypes from 'prop-types';
-import {IconClose} from '../svgs';
+import {IconClose,InformationCircled,QuestionCircle,CheckmarkCircled,CloseCircled} from '../svgs';
 
 export default class Alerts extends Component {
   state = {
@@ -15,22 +15,25 @@ export default class Alerts extends Component {
     this.props&&this.props.onClose&&this.props.onClose()
   }
   render() {
-    const { prefixCls, type, message, onClose, description, className, children, ...others } = this.props;
+    const { prefixCls, type, message, showIcon, onClose, description, className, children, ...others } = this.props;
+    let icon;
+    if(showIcon){
+      switch(type){
+        case "success": icon = CheckmarkCircled;break;
+        case "info": icon = InformationCircled;break;
+        case "warn": icon = QuestionCircle;break;
+        case "error": icon = CloseCircled;break;
+      }
+    }
     const cls = classNames(prefixCls,{
         [`${prefixCls}-${type}`]: type ,
-
-        // [`${prefixCls}-default`]: type === 'default',  //  默认样式
-        // [`${prefixCls}-primary`]: type === 'primary',  // （首选项）Primary
-        // [`${prefixCls}-success`]: type === 'success',  // （成功）Success
-        // [`${prefixCls}-info`]:    type === 'info',     // （一般信息）Info
-        // [`${prefixCls}-warn`]:    type === 'warn',     // （警告）Warning
-        // [`${prefixCls}-error`]:   type === 'error',    // （危险）Danger
-
+        [`${prefixCls}-icon`]: showIcon&&icon,
+        [`${prefixCls}-description`]: description,
         [className]: className
       });
 
     const alertsview = !this.state.visible ? null :<div { ...others } className={ cls }>
-        {message&&<span className={!description?`${prefixCls}-description`:`${prefixCls}-message`}>{message}</span>}
+        {message&&<span className={!description?`${prefixCls}-description`:`${prefixCls}-message`}>{icon}{message}</span>}
         {description&&<span className={`${prefixCls}-description`}>{description}</span>}
         {children}
         {onClose&&<a href="javascript:void(0)" onClick={this.handleClose.bind(this)} className={`${prefixCls}-close-icon`}>{IconClose}</a>}
