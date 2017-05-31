f6c0ae86e3117063f530ec04a3119ab350cf8893
jaywcjlove <398188662@qq.com>
2017-06-01 03:33:45 +0800

    添加全局样式

--- docs/index.jsx
+++ docs/index.jsx
@@ -3,6 +3,9 @@ import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import App from './page';
 
+// import './style/base.less';
+import '../src/index.less';
+
 render(<AppContainer><App /></AppContainer>, document.getElementById('app'));
 
 if (module.hot) {
