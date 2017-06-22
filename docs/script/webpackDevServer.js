46a0d85eefe9ffd296564b442cbb36e325261285
王楚江 <wcj@nihaosi.com>
2017-06-23 02:03:27 +0800

    添加注释

--- docs/script/webpackDevServer.js
+++ docs/script/webpackDevServer.js
@@ -28,9 +28,12 @@ module.exports = function(allowedHost) {
     // really know what you're doing with a special environment variable.
     disableHostCheck: process.env.DANGEROUSLY_DISABLE_HOST_CHECK === 'true',
     // Enable gzip compression of generated files.
+    // 启用gzip压缩生成的文件。
     compress: true,
     // Silence WebpackDevServer's own logs since they're generally not useful.
+    // 不显示WebpackDevServer自己的日志，因为它们通常没有用。
     // It will still show compile warnings and errors with this setting.
+    // 它仍然会显示编译警告和错误与此设置。
     clientLogLevel: 'none',
     // By default WebpackDevServer serves physical files from current directory
     // in addition to all the virtual build products that it serves from memory.
@@ -49,41 +52,54 @@ module.exports = function(allowedHost) {
     contentBase: paths.appPublic,
     
     // By default files from `contentBase` will not trigger a page reload.
+    // 默认情况下，`contentBase`的文件不会触发页面重新加载。
     watchContentBase: true,
     // Enable hot reloading server. It will provide /sockjs-node/ endpoint
     // for the WebpackDevServer client so it can learn when the files were
     // updated. The WebpackDevServer client is included as an entry point
     // in the Webpack development configuration. Note that only changes
     // to CSS are currently hot reloaded. JS changes will refresh the browser.
+    // 启用热重新加载服务器。 它将为 WebpackDevServer 客户端提供 `/sockjs-node/endpoint`，以便可以了解文件何时被更新。 
+    // WebpackDevServer 客户端作为Webpack开发配置中的入口点。 请注意，只有CSS的更改目前正在重新加载。 JS更改将刷新浏览器。
     hot: true,
     // It is important to tell WebpackDevServer to use the same "root" path
     // as we specified in the config. In development, we always serve from /.
+    // 告诉WebpackDevServer使用与我们在config中指定的相同的“root”路径是很重要的。 
+    // 在开发中，我们始终从 /。
     publicPath: '/',
     // WebpackDevServer is noisy by default so we emit custom message instead
     // by listening to the compiler events with `compiler.plugin` calls above.
+    // WebpackDevServer在默认情况下是杂乱的，所以我们自定义消息，
+    // 而不是通过上面的`compile.plugin`调用监听编译器事件。
     quiet: true,
     // Reportedly, this avoids CPU overload on some systems.
+    // 据小道消息称，这是避免了某些系统的CPU过载。
     // https://github.com/facebookincubator/create-react-app/issues/293
     watchOptions: {
       ignored: /node_modules/,
     },
     // Enable HTTPS if the HTTPS environment variable is set to 'true'
+    // 如果HTTPS环境变量设置为“true”，则启用HTTPS
     https: protocol === 'https',
     host: "127.0.0.1",
     overlay: false,
     historyApiFallback: {
       // Paths with dots should still use the history fallback.
+      // 带点`.`的路径应该仍然使用历史回退。
       // See https://github.com/facebookincubator/create-react-app/issues/387.
       disableDotRule: true,
     },
     public: allowedHost,
     setup(app) {
       // This lets us open files from the runtime error overlay.
+      // 这使我们可以从运行时错误叠加层打开文件。
       app.use(errorOverlayMiddleware());
       // This service worker file is effectively a 'no-op' that will reset any
       // previous service worker registered for the same host:port combination.
+      // 这个服务工作者文件实际上是一个“no-op”，它会重置任何在同一个主机上注册的服务 host:port 组合。
       // We do this in development to avoid hitting the production cache if
       // it used the same host and port.
+      // 我们在开发中这样做，以避免在使用相同的主机和端口时触发生产缓存。
       // https://github.com/facebookincubator/create-react-app/issues/2272#issuecomment-302832432
       app.use(noopServiceWorkerMiddleware());
     },
