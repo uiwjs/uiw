3ed100fbfc95668cf4703b6e3f8bb5d348205592
张鹏 <zp@nihaosi.com>
2017-11-13 19:18:01 +0800

    input test2

--- src/input/__test__/Input.test.js
+++ src/input/__test__/Input.test.js
@@ -4,14 +4,17 @@ import { Input } from '../../../src';
 
 
 describe('<Input>',() => {
-  const warpperState = {
+  const wrapperState = {
     value:0,
   }
   var wrapper = mount(<Input
     onChange ={(e,value)=>{
-      warpperState.value = value
+      wrapperState.value = value
+    }}  
+    onSearch ={(e,value)=>{
+      wrapperState.value = value
     }}/>);
-
+ 
 
   it('Test the default props and node.',()=>{
     expect(wrapper.name()).toBe('Input');
@@ -23,7 +26,54 @@ describe('<Input>',() => {
     expect(wrapper.at(0).prop('autoComplete')).toBe('off');
     expect(wrapper.find('.w-input').type()).toBe('div')
     expect(wrapper.find('.w-input-inner').type()).toBe('input');
-
   })
 
+
+  it('Test disabled attributes.',() => {
+    wrapper.setProps({ disabled: true });
+    expect(wrapper.find('.w-input').at(0).prop('className')).toBe('w-input w-disabled');
+  })
+
+  it('Test textarea attributes.',() => {
+    wrapper.setProps({ type: 'textarea' });
+    wrapper.setProps({ disabled: false });
+    expect(wrapper.at(0).prop('type')).toBe('textarea');
+    expect(wrapper.find('.w-input').at(0).prop('className')).toBe('w-input textarea w-input-inner');
+  })
+
+  it('Test onChange event.',()=>{
+    wrapper.setProps({ type: 'text'});
+    let input = wrapper.find('.w-input').at(0);
+    input.simulate('change')
+    expect(input.html()).toContain('<div class="w-input"><input type="text" autocomplete="off" class="w-input-inner"></div>')
+  })
+
+    it('Test onSearch event.',()=>{
+    wrapper.setProps({ type: 'input' });
+    let input = wrapper.find('.w-input').at(0);
+    input.simulate('change')
+    expect(input.html()).toContain('<div class="w-input"><input type="input" autocomplete="off" class="w-input-inner"></div>')
+  })
+
+  it('Test onKeyUp event.',()=>{
+    wrapper.setProps({ type: 'input' });
+    let input = wrapper.find('.w-input').at(0);
+    input.simulate('keyup')
+    expect(input.html()).toContain('<div class="w-input"><input type="input" autocomplete="off" class="w-input-inner"></div>')
+  })
+
+  it('Test defaultValue attributes.',() => {
+    wrapper.setProps({ defaultValue: '默认值' });
+    expect(wrapper.at(0).prop('defaultValue')).toBe('默认值');
+  })
+
+  it('Test size attributes.',() => {
+    wrapper.setProps({ size: 'mini' });
+    expect(wrapper.at(0).prop('size')).toBe('mini');
+    wrapper.setProps({ size: 'large' });
+    expect(wrapper.at(0).prop('size')).toBe('large');
+    wrapper.setProps({ size: 'small' });
+    expect(wrapper.at(0).prop('size')).toBe('small');
+  })
+ 
 })
