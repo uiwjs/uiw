759a4f5fb067a0a67a9e7848d0207ba2ec763450
jaywcjlove <398188662@qq.com>
2017-11-15 11:57:58 +0800

    feat: Extract public method scrollTop.

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,3 +3,4 @@ export { default as Component } from './Component';
 export { default as PropTypes } from './PropTypes';
 export { randomid } from './strings';
 export { ReactDOM, findDOMNode, firstChild } from './React';
+export { getScrollTop } from './scrollTop';
