69f19d6b73792b437aa3874c0a1894b3af89761c
jaywcjlove <398188662@qq.com>
2017-11-22 20:15:26 +0800

    fix(Radio): Controlled component problem.

--- src/radio/__test__/Radio.test.js
+++ src/radio/__test__/Radio.test.js
@@ -16,14 +16,14 @@ describe('<Radio>', () => {
     expect(wrapper.type()).toEqual(Radio);
     expect(wrapper.at(0).prop('prefixCls')).toBe('w-radio');
     expect(wrapper.at(0).prop('disabled')).toBe(false);
-    expect(wrapper.at(0).prop('disabled')).toBe(false);
-    expect(wrapper.at(0).prop('checked')).toBeUndefined();
+    expect(wrapper.at(0).prop('checked')).toBe(false);
+    expect(wrapper.at(0).prop('value')).toBe('');
   });
 
   it('Test value and checked and disabled attributes.', () => {
     wrapper.setProps({ 'value': "1212", checked: true, disabled: true });
     expect(wrapper.find('input').at(0).getDOMNode().checked).toBe(true)
-    expect(wrapper.find('input').at(0).getDOMNode().disabled).toBe(true)
+    expect(wrapper.find('input').at(0).getDOMNode().disabled).toEqual(true)
     expect(wrapper.find('input').at(0).getDOMNode().value).toBe("1212")
   });
 
