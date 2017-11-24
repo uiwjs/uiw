a87b13dd238412971240e003aa9f542a18d9f274
张鹏 <zp@nihaosi.com>
2017-11-24 20:15:07 +0800

    Test(Table): Add table test

---
+++ src/table/__test__/Table.rowSelection.test.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { mount, render } from 'enzyme';
+import { Table } from '../../../src';
+
+describe('Table.rowSelection', () => {
+  const wrapperState = {
+    value: 0,
+  };
+
+  const columns = [{
+    title: 'Name',
+    dataIndex: 'name',
+  }];
+
+  const data = [
+    { key: 0, name: 'Jack' },
+    { key: 1, name: 'Lucy' },
+    { key: 2, name: 'Tom' },
+    { key: 3, name: 'Jerry' },
+  ];
+
+  function createTable(props = {}) {
+    return (
+      <Table
+        columns={columns}
+        data={data}
+        rowSelection={{
+          onSelectAll:(selectDatas,checked,e)=>{
+            console.log("选择或取消选择所有选项！",selectDatas)
+          },
+          onSelect:(row, index, checked, rowsChecked, e)=>{
+            wrapperState.value = index;
+          }
+        }}
+        {...props}
+      />
+    )
+  }
+
+  it('select by checkbox', () => {
+    const wrapper = mount(createTable());
+    const checkboxes = wrapper.find('input');
+    const checkboxAll = checkboxes.last();
+    checkboxAll.simulate('change', { target: { checked: true } });
+    expect(wrapperState.value).toEqual(3);
+  });
+
+})  
+
