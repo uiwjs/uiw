239126198271a8124fc51127feba6bdd0d1ab448
jaywcjlove <398188662@qq.com>
2017-11-28 19:32:27 +0800

    fix(DatePicker): Change the default export of DatePikcer.

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ export { default as Paging } from './paging/';
 export { default as Progress } from './progress/';
 export { default as Tooltip } from './tooltip/';
 export { default as Tree } from './tree/';
-export { DatePicker } from './date-picker/';
+export { default as DatePicker } from './date-picker/';
 export { default as TimePicker } from './time-picker/';
 export { default as TimeSelect } from './time-select/';
 export { default as Timestamp } from './timestamp';
