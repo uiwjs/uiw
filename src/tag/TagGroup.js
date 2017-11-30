c8dec5e847e3747087cb56afa06ae5346f87cd43
jaywcjlove <398188662@qq.com>
2017-11-30 21:41:33 +0800

    fix(Tag.Group): Fix the wrong return value.

--- src/tag/TagGroup.js
+++ src/tag/TagGroup.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Component, PropTypes } from '../utils/';
 import Tag from './Tag';
-import CheckedTag from './CheckedTag';
 import './style/tag-group.less';
-
 
 export default class TagGroup extends Component {
   constructor(props) {
@@ -41,38 +39,42 @@ export default class TagGroup extends Component {
     }
   }
 
+  onClickTag(data, checked, e) {
+    const { options, onChange, isRadio, checkedValues } = this.props;
+    const checkedValue = options.map((item) => {
+      return item.value;
+    }).filter((value) => {
+      if (isRadio) {
+        return value === data.value;
+      }
+      let checkeds = checkedValues;
+      if (checkeds.indexOf(value) < 0 && value === data.value) {
+        checkeds.push(value);
+      } else if (checked && checkeds.indexOf(value) > -1) {
+        checkeds = checkeds.filter((val) => { return val !== data.value; });
+      }
+      return checkeds.indexOf(value) > -1;
+    });
+    onChange(e, checkedValue);
+  }
   render() {
     const { prefixCls, children, options, isRadio, checkedValues, onChange, checked, className, ...other } = this.props;
-    const cls = this.classNames({
-      [`${prefixCls}`]: true,
-      [className]: className,
-    });
-
+    const cls = this.classNames(`${prefixCls}`, className);
     return (
       <div className={cls} {...other}>
-        {
-          options.map((tags, idx) => {
-            const prop = {};
-            let label = '';
-            if (typeof (tags) === 'object') {
-              prop.color = tags.color ? tags.color : '';
-            }
-
-            if (typeof (tags) === 'string') label = tags;
-            if (tags.label || tags.value) label = tags.label || tags.value;
-
-            if (Array.isArray(checkedValues) && (checked || isRadio)) {
-              let isChecked = false;
-              if (checkedValues.indexOf(tags) > -1 || checkedValues.indexOf(tags.value) > -1 || checkedValues.indexOf(tags.label) > -1) {
-                isChecked = true;
-              }
-              return (
-                <CheckedTag data={tags} checked={isChecked} {...prop} key={idx}>{label}</CheckedTag>
-              );
-            }
-            return <Tag data={tags} {...prop} key={idx} onClose={this.handleClose.bind(this, tags)}>{label}</Tag>;
-          })
-        }
+        {options.map((item, idx) => {
+          const data = typeof item === 'string' ? { value: item, label: item } : item;
+          const prop = {
+            data,
+            color: item.color ? item.color : 'default',
+          };
+          if (checked === false || checked === true) {
+            prop.checked = checkedValues.indexOf(data.value) > -1;
+          }
+          return (
+            <Tag {...prop} key={idx} onClose={this.handleClose.bind(this, item)} onClick={this.onClickTag.bind(this, item, prop.checked)}>{prop.data.label || prop.data.value}</Tag>
+          );
+        })}
         {children &&
           <div
             className={this.classNames(`${prefixCls}-children`)}
@@ -86,7 +88,6 @@ export default class TagGroup extends Component {
     );
   }
 }
-
 
 TagGroup.childContextTypes = {
   component: PropTypes.any,
