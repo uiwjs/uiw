c9e43a601277113b0a7c0514189d167c1e944c41
jaywcjlove <398188662@qq.com>
2017-12-16 00:59:34 +0800

    fix(DatePicker): export component.

--- src/date-picker/index.js
+++ src/date-picker/index.js
@@ -2,4 +2,7 @@
 import DatePicker from './DatePicker';
 import './style/index.less';
 
+export { default as TimeSelect } from './TimeSelect';
+export { default as TimePicker } from './TimePicker';
+
 export default DatePicker;
