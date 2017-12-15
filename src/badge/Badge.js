d48cddbac0cd83ef10b440d050b8de24ae2d0d17
jaywcjlove <398188662@qq.com>
2017-12-15 20:17:09 +0800

    feat(Badge): Add status props.

--- src/badge/Badge.js
+++ src/badge/Badge.js
@@ -3,16 +3,19 @@ import { Component, PropTypes } from '../utils/';
 
 export default class Badge extends Component {
   render() {
-    const { prefixCls, style, max, dot, className, count, children, ...other } = this.props;
+    const { prefixCls, style, max, dot, status, className, count, children, ...other } = this.props;
     return (
       <div
         {...other}
         className={this.classNames(className, `${prefixCls}`, {
           nowrap: !children,
+          [`${prefixCls}-status`]: status,
+          [`${prefixCls}-status-${status}`]: status,
         })}
       >
+        {status && (<span className={`${prefixCls}-dot`} />)}
         {children}
-        {count !== 0 &&
+        {count !== 0 && !status &&
           <sup
             style={style}
             className={this.classNames({
@@ -33,10 +36,14 @@ Badge.propTypes = {
   count: PropTypes.number,
   dot: PropTypes.bool,
   max: PropTypes.number,
+  status: PropTypes.oneOf([
+    'success', 'processing', 'default', 'error', 'warning',
+  ]),
 };
 
 Badge.defaultProps = {
   prefixCls: 'w-badge',
   dot: false,
   max: 99,
+  status: null,
 };
