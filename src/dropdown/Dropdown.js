f982c4ea19a53992efb8a107bdf53f84fe25d103
jaywcjlove <398188662@qq.com>
2017-12-27 10:48:01 +0800

    feat(Dropdown): Add onVisibleChange props.

--- src/dropdown/Dropdown.js
+++ src/dropdown/Dropdown.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Component, PropTypes } from '../utils/';
+import { Component, PropTypes, ReactDOM } from '../utils/';
 import Transition from '../transition';
 
 export default class Dropdown extends Component {
@@ -8,24 +8,70 @@ export default class Dropdown extends Component {
     this.state = {
       visible: props.visible,
     };
+    this.handleClickOutside = this.handleClickOutside.bind(this);
   }
   componentWillReceiveProps(nextProps) {
     if (nextProps.visible !== this.props.visible) {
       this.setState({ visible: nextProps.visible });
     }
   }
+  componentDidMount() {
+    document.addEventListener('mousedown', this.handleClickOutside, true);
+  }
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleClickOutside, true);
+  }
+  handleClickOutside(e) {
+    const { onVisibleChange } = this.props;
+    // Ignore clicks on the component it self
+    // https://codepen.io/graubnla/pen/EgdgZm
+    // Detect a click outside of a React Component
+    // https://www.dhariri.com/posts/57c724e4d1befa66e5b8e056
+    const domNode = ReactDOM.findDOMNode(this);
+    if ((!domNode || !domNode.contains(e.target))) {
+      this.setState({ visible: false }, () => {
+        this.showDropdown = false;
+        onVisibleChange(this.state.visible);
+      });
+    }
+  }
   onClick = () => {
-    console.log('click!!');
+    const { visible, onVisibleChange } = this.props;
+    if (this.showDropdown) {
+      return;
+    }
+    this.setState({ visible: !visible }, () => {
+      this.showDropdown = true;
+      onVisibleChange(this.state.visible);
+    });
   }
   onMouseEnter = () => {
-    this.setState({ visible: true });
+    const { onVisibleChange } = this.props;
+    this.setState({ visible: true }, () => {
+      onVisibleChange(this.state.visible);
+    });
     this.leaveDelay = false;
   }
   onMouseLeave = () => {
+    const { onVisibleChange } = this.props;
     this.leaveDelay = true;
-    setTimeout(() => {
-      this.leaveDelay && this.setState({ visible: false });
+    if (this.timer) clearTimeout(this.timer);
+    // 解决闪烁出现隐藏
+    this.timer = setTimeout(() => {
+      this.leaveDelay && this.setState({ visible: false }, () => {
+        onVisibleChange(this.state.visible);
+        clearTimeout(this.timer);
+      });
     }, 300);
+  }
+  onSelectMenu = () => {
+    const { onVisibleChange } = this.props;
+    if (this.showDropdown) {
+      this.setState({ visible: false }, () => {
+        this.showDropdown = false;
+        onVisibleChange(this.state.visible);
+      });
+    }
   }
   render() {
     const { prefixCls, className, children, disabled, menu, trigger, ...resetProps } = this.props;
@@ -49,7 +95,9 @@ export default class Dropdown extends Component {
         {React.Children.map(children, (child, index) => React.cloneElement(child, { key: index }))}
         <Transition in={visible} sequence="fadeIn">
           <div className={`${prefixCls}-menu-warpper`}>
-            {menu && !disabled && React.cloneElement(menu)}
+            {menu && !disabled && React.cloneElement(menu, {
+              onSelect: this.onSelectMenu,
+            })}
           </div>
         </Transition>
       </div>
@@ -62,6 +110,7 @@ Dropdown.propTypes = {
   disabled: PropTypes.bool,
   visible: PropTypes.bool,
   menu: PropTypes.node,
+  onVisibleChange: PropTypes.func,
   trigger: PropTypes.oneOf(['click', 'hover']),
 };
 
@@ -70,4 +119,5 @@ Dropdown.defaultProps = {
   trigger: 'hover',
   menu: null,
   visible: false,
+  onVisibleChange() { },
 };
