793a7e24dc297cef788a52924b4da81763102aa7
jaywcjlove <398188662@qq.com>
2018-05-09 09:43:16 +0800

    Update deploy command.

--- script/deploy.js
+++ script/deploy.js
@@ -5,10 +5,12 @@ const loading = require('loading-cli');
 require('colors-cli/toxic');
 const pkg = require('../package.json');
 
+const log = console.log;
+
 const ORIGIN = 'git@github.com:uiw-react/uiw-react.github.io.git';
 // const BRANCH = 'gh-pages';
-console.log('  Start public to your git repo'.green); // eslint-disable-line
-console.log(`  ${ORIGIN}\n`.green); // eslint-disable-line
+log('  Start public to your git repo'.green);
+log(`  ${ORIGIN}\n`.green);
 
 function pushDirToBranch(dir, branch, callback) {
   const load = loading({
@@ -25,15 +27,15 @@ function pushDirToBranch(dir, branch, callback) {
   }, (err) => {
     load.stop();
     if (err) {
-      return console.log(err); // eslint-disable-line
+      return log(err);
     }
-    console.log(`\n  Push to ${branch} success!\n`.green.bold); // eslint-disable-line
+    log(`\n  Push to ${branch} success!\n`.green.bold);
     if (callback && typeof callback === 'function') callback();
   });
 }
 
 del(['node_modules/gh-pages/.cache/**']).then(() => {
-  pushDirToBranch('dist', 'gh-pages', () => {
-    console.log(`\n  Push to ${branch} done!\n`.green.bold); // eslint-disable-line
+  pushDirToBranch('dist', 'master', () => {
+    log(`\n  Push to ${branch} done!\n`.green.bold);
   });
 });
