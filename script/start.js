b931d803f1f16cd3822565b55d90755deacf0b53
jaywcjlove <398188662@qq.com>
2018-06-22 18:05:38 +0800

    chore: Modify the URL display in the command line.

--- script/start.js
+++ script/start.js
@@ -2,6 +2,7 @@ import webpack from 'webpack';
 import detect from 'detect-port';
 import openBrowsers from 'open-browsers';
 import WebpackDevServer from 'webpack-dev-server';
+import prepareUrls from 'local-ip-url/prepareUrls';
 import load from 'loading-cli';
 import 'colors-cli/toxic';
 import conf from './conf/webpack.config.dev';
@@ -13,16 +14,22 @@ const compiler = webpack(conf);
 const loading = load('Compiler is running...'.green).start();
 loading.color = 'green';
 
-// https://webpack.js.org/api/compiler-hooks/#aftercompile
-// 编译完成之后打印日志
-compiler.hooks.done.tap('done', () => {
-  loading.stop();
-  // eslint-disable-next-line
-  console.log(`\nDev Server Listening at ${`http://${HOST}:${PORT}`.green}`);
-});
 
 detect(PORT).then((_port) => {
   if (PORT !== _port) PORT = _port;
+
+  const protocol = process.env.HTTPS === 'true' ? 'https' : 'http';
+  const urls = prepareUrls({ protocol, host: HOST, port: PORT });
+  // https://webpack.js.org/api/compiler-hooks/#aftercompile
+  // 编译完成之后打印日志
+  compiler.hooks.done.tap('done', () => {
+    loading.stop();
+    /* eslint-disable */
+    console.log(`Dev Server Listening at Local: ${urls.localUrl.green}`);
+    console.log(`              On Your Network: ${urls.lanUrl.green}`);
+    /* eslint-enable */
+  });
+
   const devServer = new WebpackDevServer(compiler, {
     // contentBase: conf.output.appPublic,
     publicPath: conf.output.publicPath,
@@ -35,7 +42,7 @@ detect(PORT).then((_port) => {
       return console.log(err); // eslint-disable-line
     }
     // open browser
-    openBrowsers(`http://${HOST}:${PORT}`);
+    openBrowsers(urls.localUrl);
 
     ['SIGINT', 'SIGTERM'].forEach((sig) => {
       process.on(sig, () => {
