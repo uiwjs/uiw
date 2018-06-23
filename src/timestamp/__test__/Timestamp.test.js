547861893896d45f35b517d6f97487bfb14cd14a
jaywcjlove <398188662@qq.com>
2018-06-23 17:33:47 +0800

    test(Switch): Add test case.

--- src/timestamp/__test__/Timestamp.test.js
+++ src/timestamp/__test__/Timestamp.test.js
@@ -5,6 +5,7 @@ import { Timestamp } from '../../../src';
 describe('<Timestamp>', () => {
   const wrapper = mount(<Timestamp
     value="Sat Jun 23 2018 17:13:01 GMT+0800"
+    tzc={8}
   />);
   it('Test the default props and node.', () => {
     expect(wrapper.name()).toBe('Timestamp');
@@ -13,6 +14,7 @@ describe('<Timestamp>', () => {
     expect(wrapper.type()).toEqual(Timestamp);
     expect(wrapper.at(0).prop('prefixCls')).toBe('w-timestamp');
     expect(wrapper.at(0).prop('format')).toBe('Y-m-d h:i:s');
+    expect(wrapper.at(0).prop('tzc')).toBe(8);
     expect(wrapper.html()).toContain('<span class="w-timestamp" interval="1000">2018-06-23 05:13:01</span>');
   });
 });
