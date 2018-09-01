be2e6b6b650d7ed4d9c5ca68623d2357045cbeb8
jaywcjlove <398188662@qq.com>
2018-09-01 17:12:42 +0800

    refactor(Breadcrumb): Optimization code.

--- src/breadcrumb/BreadcrumbItem.js
+++ src/breadcrumb/BreadcrumbItem.js
@@ -4,19 +4,16 @@ import { Component, PropTypes } from '../utils/';
 export default class BreadcrumbItem extends Component {
   render() {
     const { prefixCls, href, separator, className, ...other } = this.props;
-    if (href) {
-      return (
-        <span className={this.classNames(`${prefixCls}`, className)}>
-          <a href={href} {...other} className={`${prefixCls}-link`}>
-            <span className={`${prefixCls}-inner`}>{this.props.children}</span>
-            <span className={`${prefixCls}-separator`}>{separator || this.context.separator}</span>
-          </a>
-        </span>
-      );
-    }
     return (
       <span {...other} className={this.classNames(prefixCls, className)}>
-        <span className={`${prefixCls}-inner`}>{this.props.children}</span>
+        <span className={`${prefixCls}-inner`}>
+          {href && (
+            <a href={href} {...other} className={`${prefixCls}-link`}>
+              {this.props.children}
+            </a>
+          )}
+          {!href && this.props.children}
+        </span>
         <span className={`${prefixCls}-separator`}>{separator || this.context.separator}</span>
       </span>
     );
