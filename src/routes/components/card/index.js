464ab85569755c01beddbbf2c206b2d4c0057f49
jaywcjlove <398188662@qq.com>
2018-12-28 15:45:53 +0800

    Update the Card component document.

--- src/routes/components/card/index.js
+++ src/routes/components/card/index.js
@@ -1,10 +1,10 @@
-import { Card } from '@uiw/core';
+import { Card, Icon } from '@uiw/core';
 import Markdown from '../../../components/Markdown';
 
 
 export default class Page extends Markdown {
   path = 'src/routes/components/card/index.md';
-  dependencies = { Card };
+  dependencies = { Card, Icon };
   async renderPage() {
     const md = await import('../../../../packages/core/src/card/index.md');
     return md.default || md;
