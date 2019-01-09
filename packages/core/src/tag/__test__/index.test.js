284081fc9758adadf35a2ecba49edcd8d8454a3d
visnix <quantumprogr@gmail.com>
2019-01-09 20:11:26 +0800

    Add Tag component test case

---
+++ packages/core/src/tag/__test__/index.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TestUtils, { isCompositeComponent, isDOMComponent, renderIntoDocument } from 'react-dom/test-utils'; // ES6
+import renderer from 'react-test-renderer';
+import Tag from '../';
+
+describe('<Tag />', () => {
+  test('Basic component loading', () => {
+    let component = renderer.create(<Tag />);
+    let tree = component.toJSON();
+    expect(isCompositeComponent(component.getInstance())).toBe(true)
+  })
+})
