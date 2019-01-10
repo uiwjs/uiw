22e33590bc972364a39268bfb3fa2bfbc4577a44
xingyuefeng <15262870437@163.com>
2019-01-10 20:46:12 +0800

    Badge: Add test case

--- packages/core/src/badge/__test__/index.test.js
+++ packages/core/src/badge/__test__/index.test.js
@@ -5,9 +5,14 @@ import { findDOMNode } from 'react-dom';
 import { Badge } from '..';
 
 describe('<Badge />', () => {
-  test('Basic component loading', () => {
+  test('Basic component loading1', () => {
     const component = renderIntoDocument(<Badge count={10} />);
     const count = Number(findRenderedDOMComponentWithClass(component, 'w-badge-count').innerHTML);
     expect(count).toBe(10);
+  });
+  test('Basic component loading2', () => {
+    const component = renderIntoDocument(<Badge count={10} max={9} />);
+    const count = findRenderedDOMComponentWithClass(component, 'w-badge-count').innerHTML;
+    expect(count).toBe('9+');
   });
 });
