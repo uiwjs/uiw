a969dc69c1e40c0899085bf3ac4fd0012bfb3bd4
jaywcjlove <398188662@qq.com>
2019-01-18 01:51:44 +0800

    doc: The document website displys the version.

---
+++ src/components/VersionSelect/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Select } from '@uiw/core';
+import styles from './index.module.less';
+
+export default class VersionSelect extends Component {
+  onChange(e) {
+    const version = e.target.value;
+    const isV1 = version.split('.')[0] === '1';
+    if (isV1) {
+      window.location.href = `https://unpkg.com/uiw@${version}/dist/index.html`;
+    } else {
+      window.location.href = `https://unpkg.com/@uiw/core@${version}/docs/index.html`;
+    }
+  }
+  render() {
+    const { data } = this.props;
+    return (
+      <div className={styles.nav}>
+        <Select defaultValue={data[0]} onChange={this.onChange.bind(this)}>
+          {data.map((version, idx) => {
+            return (
+              <Select.Option key={idx} value={version}>{version}</Select.Option>
+            );
+          })}
+        </Select>
+      </div>
+    );
+  }
+}
