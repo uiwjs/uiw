872950c75edff623254f352bcf253489605d2b53
jaywcjlove <398188662@qq.com>
2019-01-18 01:48:56 +0800

    Modify webpack config.

--- .kktrc.js
+++ .kktrc.js
@@ -5,42 +5,39 @@ module.exports = {
   // Modify the webpack config
   config: (conf, { dev, env }, webpack) => {
     if (env === 'prod') {
-      // conf = {
-      //   ...conf,
-      //   optimization: {
-      //     ...conf.optimization,
-      //     // https://webpack.js.org/plugins/split-chunks-plugin/
-      //     splitChunks: {
-      //       chunks: 'async',
-      //       minSize: 30000,
-      //       minChunks: 2,
-      //       maxAsyncRequests: 5,
-      //       maxInitialRequests: 3,
-      //       automaticNameDelimiter: '~',
-      //       name: true,
-      //       cacheGroups: {
-      //         vendors: {
-      //           test: /[\\/]node_modules[\\/]/,
-      //           priority: -10
-      //         },
-      //         default: {
-      //           minChunks: 2,
-      //           priority: -20,
-      //           reuseExistingChunk: true
-      //         }
-      //       }
-      //     }
-      //   }
-      // };
+      conf.output.publicPath = './';
+      conf = {
+        ...conf,
+        optimization: {
+          ...conf.optimization,
+          // https://webpack.js.org/plugins/split-chunks-plugin/
+          splitChunks: {
+            chunks: 'async',
+            minSize: 30000,
+            minChunks: 2,
+            maxAsyncRequests: 5,
+            maxInitialRequests: 3,
+            automaticNameDelimiter: '~',
+            name: true,
+            cacheGroups: {
+              vendors: {
+                test: /[\\/]node_modules[\\/]/,
+                priority: -10
+              },
+              default: {
+                minChunks: 2,
+                priority: -20,
+                reuseExistingChunk: true
+              }
+            }
+          }
+        }
+      };
     }
     conf.module.rules = [
       {
         test: /\.md$/,
-        use: [
-          {
-            loader: require.resolve('raw-loader'),
-          },
-        ]
+        loader: require.resolve('raw-loader')
       },
       ...conf.module.rules,
     ]
