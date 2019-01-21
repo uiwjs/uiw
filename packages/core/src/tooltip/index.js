f22c1071d365ca07567bd7f3af37a0dc036ba11a
jaywcjlove <398188662@qq.com>
2019-01-22 02:48:32 +0800

    feat(Tooltip): Simplify how components are used.

--- packages/core/src/tooltip/index.js
+++ packages/core/src/tooltip/index.js
@@ -1,25 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import OverlayTrigger from '../overlay-trigger';
 import './style/index.less';
 
 export default class Tooltip extends React.Component {
   render() {
-    const { prefixCls, className, placement, visibleArrow, ...other } = this.props;
+    const { prefixCls, className, placement, visible, trigger, delay, visibleArrow, onVisibleChange, ...other } = this.props;
     const cls = classnames(prefixCls, className, {
       [`${placement}`]: placement,
     });
     return (
-      <div className={cls} {...other}>
-        {visibleArrow && <div className={`${prefixCls}-arrow`} />}
-        <div className={`${prefixCls}-inner`}>{this.props.children}</div>
-      </div>
+      <OverlayTrigger
+        usePortal={false}
+        visible={visible}
+        trigger={trigger}
+        delay={delay}
+        onVisibleChange={onVisibleChange}
+        placement={placement}
+        overlay={
+          <div className={cls} {...other}>
+            {visibleArrow && <div className={`${prefixCls}-arrow`} />}
+            <div className={`${prefixCls}-inner`}>{this.props.content}</div>
+          </div>
+        }
+      >
+        {typeof this.props.children === 'object' ? this.props.children : <span>{this.props.children}</span>}
+      </OverlayTrigger>
     );
   }
 }
 
 Tooltip.propTypes = {
   prefixCls: PropTypes.string,
+  visible: PropTypes.bool,
+  content: PropTypes.oneOfType([PropTypes.node, PropTypes.string]).isRequired,
+  delay: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.shape({
+      show: PropTypes.number,
+      hide: PropTypes.number,
+    }),
+  ]),
   // Tooltip 的出现位置
   placement: PropTypes.oneOf([
     'top', 'topLeft', 'topRight',
@@ -34,5 +56,6 @@ Tooltip.propTypes = {
 Tooltip.defaultProps = {
   prefixCls: 'w-tooltip',
   placement: 'top',
+  visible: false,
   visibleArrow: true,
 };
