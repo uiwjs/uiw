415fee22480f052bdfc9904bbf9adeba95c3b621
jaywcjlove <398188662@qq.com>
2019-01-22 15:03:01 +0800

    docs(Tooltip): Update document.

--- src/routes/components/tooltip/index.js
+++ src/routes/components/tooltip/index.js
@@ -1,9 +1,9 @@
-import { Tooltip, OverlayTrigger, Button, Divider } from '@uiw/core';
+import { Tooltip, OverlayTrigger, Switch, Button, Divider } from '@uiw/core';
 import Markdown from '../../../components/Markdown';
 
 export default class Page extends Markdown {
   path = 'src/routes/components/tooltip/README.md';
-  dependencies = { Tooltip, OverlayTrigger, Button, Divider };
+  dependencies = { Tooltip, OverlayTrigger, Switch, Button, Divider };
   async renderPage() {
     const md = await import('../../../../packages/core/src/tooltip/README.md');
     return md.default || md;
