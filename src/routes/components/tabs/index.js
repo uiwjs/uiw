caa66d482c7f251f0f36846c13e85ec5bdac1481
jaywcjlove <398188662@qq.com>
2019-02-11 00:26:15 +0800

    doc(Tabs): Add example.

--- src/routes/components/tabs/index.js
+++ src/routes/components/tabs/index.js
@@ -1,10 +1,10 @@
-import { Tabs, Button } from 'uiw';
+import { Tabs, Divider } from 'uiw';
 import Markdown from '../../../components/Markdown';
 
 
 export default class Page extends Markdown {
   path = 'packages/core/src/tabs/README.md';
-  dependencies = { Tabs, Button };
+  dependencies = { Tabs, Divider };
   async renderPage() {
     const md = await import('../../../../packages/core/src/tabs/README.md');
     return md.default || md;
