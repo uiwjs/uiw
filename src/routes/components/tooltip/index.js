cdc88e02483db3f4100600da893a4f419ef42cb6
jaywcjlove <398188662@qq.com>
2019-02-12 15:09:19 +0800

    fix(doc): Fix document menu tooltip issue.

--- src/routes/components/tooltip/index.js
+++ src/routes/components/tooltip/index.js
@@ -1,9 +1,9 @@
-import { Tooltip, OverlayTrigger, Switch, Button, Divider } from 'uiw';
+import { Tooltip, OverlayTrigger, Switch, Button, Input, Divider } from 'uiw';
 import Markdown from '../../../components/Markdown';
 
 export default class Page extends Markdown {
   path = 'packages/core/src/tooltip/README.md';
-  dependencies = { Tooltip, OverlayTrigger, Switch, Button, Divider };
+  dependencies = { Tooltip, OverlayTrigger, Switch, Button, Input, Divider };
   async renderPage() {
     const md = await import('../../../../packages/core/src/tooltip/README.md');
     return md.default || md;
