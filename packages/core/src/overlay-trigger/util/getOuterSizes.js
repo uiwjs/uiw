00eeaa90a0a9905b2341606036e778027e0cf98c
jaywcjlove <398188662@qq.com>
2019-02-12 15:28:33 +0800

    fix(OverlayTrigger): Fix svg width calculation issue.

--- packages/core/src/overlay-trigger/util/getOuterSizes.js
+++ packages/core/src/overlay-trigger/util/getOuterSizes.js
@@ -11,8 +11,8 @@ export default function getOuterSizes(element) {
   // const x = parseFloat(styles.marginTop || 0) + parseFloat(styles.marginBottom || 0);
   // const y = parseFloat(styles.marginLeft || 0) + parseFloat(styles.marginRight || 0);
   const result = {
-    width: element.offsetWidth,
-    height: element.offsetHeight,
+    width: element.offsetWidth || element.scrollWidth || 0,
+    height: element.offsetHeight || element.scrollHeight || 0,
   };
   return result;
 }
