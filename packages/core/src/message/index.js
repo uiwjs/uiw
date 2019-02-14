4e7bd49d38a81351eaa5a30e24d2eccede134ca7
jaywcjlove <398188662@qq.com>
2019-02-15 01:09:07 +0800

    feat(Message): Add title/description/showIcon/icon props.

--- packages/core/src/message/index.js
+++ packages/core/src/message/index.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import Icon from '../icon';
 import './style/index.less';
 
 export default class Message extends React.Component {
+  renderIcon = () => {
+    const { type, showIcon } = this.props;
+    let icon = this.props.icon;
+    if (!icon && showIcon) {
+      switch (type) {
+        case 'success': icon = 'circle-check'; break;
+        case 'warning': icon = 'warning'; break;
+        case 'info': icon = 'information'; break;
+        case 'error': icon = 'circle-close'; break;
+        default: break;
+      }
+    }
+    return icon;
+  }
   render() {
-    const { prefixCls, className, type, title, description, ...elementProps } = this.props;
-    const cls = classnames(prefixCls, className, `${prefixCls}-${type}`);
+    const { prefixCls, className, type, title, description, showIcon, icon, rounded, ...elementProps } = this.props;
+    const children = description || this.props.children;
+    const cls = classnames(prefixCls, className, `${prefixCls}-${type}`, {
+      [`${prefixCls}-rounded`]: rounded,
+      [`${prefixCls}-icon`]: showIcon,
+      [`${prefixCls}${title ? '-title' : ''}${children ? '-description' : ''}`]: showIcon,
+    });
     return (
       <div className={cls} {...elementProps}>
+        {showIcon && <Icon type={this.renderIcon()} />}
         <span className={classnames(`${prefixCls}-title`)}>{title}</span>
-        <span className={classnames(`${prefixCls}-description`)}>{description}</span>
+        <span className={classnames(`${prefixCls}-description`)}>{children}</span>
       </div>
     );
   }
@@ -18,6 +39,8 @@ export default class Message extends React.Component {
 
 Message.propTypes = {
   prefixCls: PropTypes.string,
+  showIcon: PropTypes.bool,
+  rounded: PropTypes.bool,
   title: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.element,
@@ -27,9 +50,9 @@ Message.propTypes = {
     PropTypes.element,
   ]),
   type: PropTypes.oneOf(['success', 'warning', 'info', 'error']).isRequired,
-  
 };
 
 Message.defaultProps = {
   prefixCls: 'w-message',
+  rounded: true,
 };
--- src/routes/components/message/index.js
+++ src/routes/components/message/index.js
@@ -1,10 +1,10 @@
-import { Message } from 'uiw';
+import { Message, Divider } from 'uiw';
 import Markdown from '../../../components/Markdown';
 
 
 export default class Page extends Markdown {
   path = 'packages/core/src/message/README.md';
-  dependencies = { Message };
+  dependencies = { Message, Divider };
   async renderPage() {
     const md = await import('../../../../packages/core/src/message/README.md');
     return md.default || md;
