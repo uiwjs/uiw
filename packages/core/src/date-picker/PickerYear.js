b6a0e8512f2bccb1a623f5a0f84848ea687283f3
jaywcjlove <398188662@qq.com>
2019-02-16 16:12:43 +0800

    style(DatePicker): Modify month and year panel styles.

--- packages/core/src/date-picker/PickerYear.js
+++ packages/core/src/date-picker/PickerYear.js
@@ -53,6 +53,6 @@ PickerYear.propTypes = {
 
 PickerYear.defaultProps = {
   prefixCls: 'w-datepicker',
-  panelNum: [-7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7],
+  panelNum: [-7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
   onSelected() { },
 };
