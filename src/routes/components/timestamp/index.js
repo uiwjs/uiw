73bc5d0df83fa32efbd6e03adc6d9aa48b906483
jaywcjlove <398188662@qq.com>
2019-02-17 03:28:37 +0800

    refactor(Timestamp): Modify Timestamp component.

--- src/routes/components/timestamp/index.js
+++ src/routes/components/timestamp/index.js
@@ -1,9 +1,9 @@
-import { Timestamp, Divider, Icon } from 'uiw';
+import { Timestamp, Divider, Tag, Icon } from 'uiw';
 import Markdown from '../../../components/Markdown';
 
 export default class Page extends Markdown {
   path = 'packages/core/src/timestamp/README.md';
-  dependencies = { Timestamp, Divider, Icon };
+  dependencies = { Timestamp, Divider, Tag, Icon };
   async renderPage() {
     const md = await import('../../../../packages/core/src/timestamp/README.md');
     return md.default || md;
