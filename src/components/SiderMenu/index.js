c22b15a190a4542d803cf7d0002f1e534f1c48d3
jaywcjlove <398188662@qq.com>
2019-02-19 11:39:16 +0800

    doc: Update the document white theme.

--- src/components/SiderMenu/index.js
+++ src/components/SiderMenu/index.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import classnames from 'classnames';
 import styles from './index.module.less';
 import nav from '../icons/nav';
 
 export default class SiderMenu extends Component {
   render() {
-    const { location } = this.props;
+    const { location, topmenu } = this.props;
     const path = location.pathname.split('/')[1];
     const navData = this.props.menuData.filter(item => item.path === `/${path}`)[0];
     if (!navData) {
       return null;
     }
     return (
-      <div className={styles.wapper}>
+      <div className={classnames(styles.wapper, { [`${styles.topmenu}`]: topmenu })}>
         <h2 className={styles.title}>{nav[navData.icon]}<span>{navData.name}</span></h2>
         <div className={styles.menu}>
           {navData.children && navData.children.map((item, idx) => {
