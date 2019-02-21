4748ca640d30e3d00a6284057f89778488110059
jaywcjlove <398188662@qq.com>
2019-02-22 02:13:43 +0800

    doc: Rename document index.md => README.md

--- src/routes/guide/recommendation/index.js
+++ src/routes/guide/recommendation/index.js
@@ -1,9 +1,9 @@
 import Markdown from '../../../components/Markdown';
 
 export default class Page extends Markdown {
-  path = 'src/routes/guide/recommendation/index.md';
+  path = 'src/routes/guide/recommendation/README.md';
   async renderPage() {
-    const md = await import('./index.md');
+    const md = await import('./README.md');
     return md.default || md;
   }
 }
