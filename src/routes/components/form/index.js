79f982e4640110ed77b50e36f293a144d1685a81
jaywcjlove <398188662@qq.com>
2019-02-23 00:04:27 +0800

    style(Form): Modify form help style.

--- src/routes/components/form/index.js
+++ src/routes/components/form/index.js
@@ -1,12 +1,12 @@
 import {
   Form, FormItem, Row, Col, Divider, Button, Input, Checkbox,
-  Switch, Radio, RadioGroup, Select, Textarea,
+  Switch, Radio, RadioGroup, Select, Textarea, Notify,
 } from 'uiw';
 import Markdown from '../../../components/Markdown';
 
 export default class Page extends Markdown {
   path = 'packages/core/src/form/README.md';
-  dependencies = { Form, FormItem, Row, Col, Divider, Button, Input, Checkbox, Switch, Radio, RadioGroup, Select, Textarea };
+  dependencies = { Form, FormItem, Row, Col, Divider, Button, Input, Checkbox, Switch, Radio, RadioGroup, Select, Textarea, Notify };
   async renderPage() {
     const md = await import('../../../../packages/core/src/form/README.md');
     return md.default || md;
