889c75db19a6918c4676949d97db6eba1c4f0836
jaywcjlove <398188662@qq.com>
2019-02-25 02:16:05 +0800

    doc(TimePicker): Add a demo in the form.

--- src/routes/components/time-picker/index.js
+++ src/routes/components/time-picker/index.js
@@ -1,9 +1,9 @@
-import { TimePicker, Notify, Button, Form, Row, Col } from 'uiw';
+import { TimePicker, Timestamp, Notify, Button, Form, Row, Col } from 'uiw';
 import Markdown from '../../../components/Markdown';
 
 export default class Page extends Markdown {
   path = 'packages/core/src/time-picker/README.md';
-  dependencies = { TimePicker, Notify, Button, Form, Row, Col };
+  dependencies = { TimePicker, Timestamp, Notify, Button, Form, Row, Col };
   async renderPage() {
     const md = await import('../../../../packages/core/src/time-picker/README.md');
     return md.default || md;
