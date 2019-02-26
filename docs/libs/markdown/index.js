423ca981a2d468b884bd04d9a87700f6584c47fc
wangchuxi <379925517@qq.com>
2019-02-26 15:23:47 +0800

    fix(website): Fix url errors.

--- docs/libs/markdown/index.js
+++ docs/libs/markdown/index.js
@@ -62,7 +62,7 @@ export default class Markdown extends React.Component {
         <div>
           <div className={`${prefixCls}-content-warpper`} dangerouslySetInnerHTML={{ __html: html }} />
           <div className={`${prefixCls}-docinfo`}>
-            犯了错误还是想对文件做出贡献？ <a href={`https://github.com/uiw-react/uiw/blob/master/docs/md/${this.getLang() + '/' + this.getPageName()}.md`} target="_blank" rel="noopener noreferrer">在Github上编辑本页！</a> <br />
+            犯了错误还是想对文件做出贡献？ <a href={`https://github.com/uiw-react/uiw/blob/v1/docs/md/${this.getLang() + '/' + this.getPageName()}.md`} target="_blank" rel="noopener noreferrer">在Github上编辑本页！</a> <br />
             <a href="https://github.com/uiw-react/uiw/issues" target="_blank" rel="noopener noreferrer">反馈建议</a> | <a target="_blank" rel="noopener noreferrer" href="https://github.com/uiw-react/uiw/issues/new">提交bug</a> | <a target="_blank" rel="noopener noreferrer" href="https://github.com/uiw-react/uiw">Github</a>
           </div>
         </div>
