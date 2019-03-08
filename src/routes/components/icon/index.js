6da28832d300165cb2371455140b520ae8c407a9
jaywcjlove <398188662@qq.com>
2019-03-09 01:31:15 +0800

    doc(Icon): fix search error.

--- src/routes/components/icon/index.js
+++ src/routes/components/icon/index.js
@@ -1,9 +1,9 @@
-import { Icon } from 'uiw';
+import { Icon, CopyToClipboard, Notify, Input } from 'uiw';
 import Markdown from '../../../components/Markdown';
 
 export default class Page extends Markdown {
   path = 'packages/core/src/colors/README.md';
-  dependencies = { Icon };
+  dependencies = { Icon, CopyToClipboard, Notify, Input };
   async renderPage() {
     const md = await import('../../../../packages/core/src/icon/README.md');
     return md.default || md;
