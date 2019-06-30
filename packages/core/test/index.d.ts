a3ef97d8f3213217d5b80385e47e808f3cbf2924
jaywcjlove <398188662@qq.com>
2019-07-01 00:13:27 +0800

    test: Add TypeScript declare file.

---
+++ packages/core/test/index.d.ts
@@ -0,0 +1,5 @@
+import jest from 'jest';
+
+declare var expect: jest.Expect;
+declare var describe: jest.Describe;
+declare var it: jest.It;
--- packages/core/test/tsd.d.ts
+++
@@ -1,3 +0,0 @@
-declare const describe: any;
-declare const expect: any;
-declare const it: any;
