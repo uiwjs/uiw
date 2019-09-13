079801fba7666d158469b76047ab6dee11fbb34c
jaywcjlove <398188662@qq.com>
2019-09-13 12:39:02 +0800

    feat(List): Add child nodes does note have filter.

--- src/list/index.tsx
+++ src/list/index.tsx
@@ -32,9 +32,11 @@ export default class List extends React.Component<IListProps> {
     } else {
       items = children;
     }
-    const childrenList = React.Children.map(items, (child: React.ReactNode, index) => React.cloneElement((child as React.ReactElement), {
-      key: index,
-    }));
+    const childrenList = React.Children.map(items, (child: React.ReactNode, index) => (
+      React.isValidElement(child) && React.cloneElement((child as React.ReactElement), {
+        key: index,
+      })
+    )).filter(Boolean);
     const classString = classnames(`${prefixCls}`, className, {
       [`${prefixCls}-striped`]: striped,
       [`${prefixCls}-bordered`]: bordered,
