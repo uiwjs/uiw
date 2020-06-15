494d19aa303f75d477e9b0fb2d0555fbf5876e97
jaywcjlove <398188662@qq.com>
2020-06-15 17:43:09 +0800

    chore:(Icon): Optimize the code.

--- packages/react-icon/src/index.tsx
+++ packages/react-icon/src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import classnames from 'classnames';
 import svgPaths from '@uiw/icons/fonts/w-icon.json';
 import './style/index.less';
@@ -15,45 +15,47 @@ export interface IconProps<T> extends React.HTMLAttributes<HTMLElement> {
    */
   tagName?: keyof JSX.IntrinsicElements | any;
   type?: IconsName | null | T;
-  spin: boolean;
+  spin?: boolean;
   color?: string;
   verticalAlign?: 'middle' | 'baseline';
 }
 
-export default class Icon<T> extends React.PureComponent<IconProps<T>> {
-  public static defaultProps: IconProps<{}> = {
-    prefixCls: 'w-icon',
-    verticalAlign: 'middle',
-    tagName: 'span',
-    spin: false,
-  }
-  renderSvgPaths = (type: IconsName) => {
-    const svgPathsData = svgPaths;
-    const pathStrings: string[] = svgPathsData[type];
-    if (pathStrings == null) {
-      return null;
-    }
-    return pathStrings.map((d, i) => <path key={i} d={d} fillRule="evenodd" />);
-  }
+export default function Icon<T>(props: IconProps<T> = {}) {
+  const {
+    className,
+    prefixCls = 'w-icon',
+    verticalAlign = 'middle',
+    tagName: TagName = 'span',
+    color,
+    type,
+    spin = false,
+    ...others
+  } = props;
 
-  public render() {
-    const { prefixCls, className, color, type, spin, verticalAlign, tagName: TagName, ...others } = this.props;
-    let svg = null;
-    if (typeof type === 'string') {
-      svg = <svg fill={color} viewBox="0 0 20 20">{this.renderSvgPaths(type as IconsName)}</svg>;
-    } else if (React.isValidElement(type)) {
-      svg = React.cloneElement(type as unknown as React.ReactElement, {
-        fill: color,
-      });
-    } else {
-      return null;
+  const renderSVGPaths = useMemo(() => {
+    if (!type) {
+      return null
     }
-    others.style = { fill: 'currentColor', ...others.style };
-    const propps = { ...others,
-      className: classnames(prefixCls, className, `${prefixCls}-${verticalAlign}`, { [`${prefixCls}-spin`]: spin }),
-    };
-    return (
-      <TagName {...propps}>{svg}</TagName>
-    );
+    const svgPathsData = svgPaths;
+    const pathStrings: string[] = svgPathsData[type as IconsName] || [];
+    return pathStrings.map((d, i) => <path key={i} d={d} fillRule="evenodd" />);
+  }, [type]);
+
+  let svg = null;
+  if (typeof type === 'string') {
+    svg = <svg fill={color} viewBox="0 0 20 20">{renderSVGPaths}</svg>;
+  } else if (React.isValidElement(type)) {
+    svg = React.cloneElement(type as unknown as React.ReactElement, {
+      fill: color,
+    });
+  } else {
+    return null;
   }
+  others.style = { fill: 'currentColor', ...others.style };
+  const propps = { ...others,
+    className: classnames(prefixCls, className, `${prefixCls}-${verticalAlign}`, { [`${prefixCls}-spin`]: spin }),
+  };
+  return (
+    <TagName {...propps}>{svg}</TagName>
+  );
 }
