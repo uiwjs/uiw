a76eb73b249f8abfff7d8504a2d84f7de00f8383
jaywcjlove <398188662@qq.com>
2020-06-16 15:54:02 +0800

    chore(Tabs): Optimize the code.

--- packages/react-tabs/src/Pane.tsx
+++ packages/react-tabs/src/Pane.tsx
@@ -4,14 +4,9 @@ import { IProps, HTMLDivProps } from '@uiw/utils';
 
 export interface TabsPaneProps extends IProps, HTMLDivProps {}
 
-export default class Pane extends React.Component<TabsPaneProps> {
-  public static defaultProps: TabsPaneProps = {
-    prefixCls: 'w-tabs-pane',
-  }
-  render() {
-    const { prefixCls, className, ...resetProps } = this.props;
-    return (
-      <div className={classnames(`${prefixCls}`, className)} {...resetProps} />
-    );
-  }
+export default (props: TabsPaneProps = {}) => {
+  const { prefixCls = 'w-tabs-pane', className, ...resetProps } = props;
+  return (
+    <div className={classnames(`${prefixCls}`, className)} {...resetProps} />
+  );
 }
