de13315d9e4f144301f04d63a7a70ef3261c721e
jaywcjlove <398188662@qq.com>
2020-06-21 01:17:24 +0800

    chore(Menu): Optimize the code.

--- website/uiw/.kktrc.js
+++ website/uiw/.kktrc.js
@@ -1,23 +1,33 @@
-import path from 'path';
+import path from "path";
 
 export const loaderOneOf = [
-  require.resolve('@kkt/loader-less'),
-  require.resolve('@kkt/loader-raw'),
+  require.resolve("@kkt/loader-less"),
+  require.resolve("@kkt/loader-raw"),
 ];
 
 export default (conf, { paths }, webpack) => {
   conf.resolve.alias = {
-    '@': paths.appSrc,
+    "@": paths.appSrc,
+    react: path.resolve("./node_modules/react"),
   };
-  const pkg = require(path.resolve(process.cwd(), 'node_modules/uiw/package.json'));
+  const pkg = require(path.resolve(
+    process.cwd(),
+    "node_modules/uiw/package.json"
+  ));
 
   const regexp = /^(ModuleScopePlugin)/;
-  conf.resolve.plugins = conf.resolve.plugins.map((item) => {
-    if (item.constructor && item.constructor.name && regexp.test(item.constructor.name)) {
-      return null;
-    }
-    return item;
-  }).filter(Boolean);
+  conf.resolve.plugins = conf.resolve.plugins
+    .map((item) => {
+      if (
+        item.constructor &&
+        item.constructor.name &&
+        regexp.test(item.constructor.name)
+      ) {
+        return null;
+      }
+      return item;
+    })
+    .filter(Boolean);
 
   // 获取版本
   conf.plugins.push(
@@ -25,7 +35,6 @@ export default (conf, { paths }, webpack) => {
       VERSION: JSON.stringify(pkg.version),
     })
   );
-  conf.output = { ...conf.output, publicPath: './' }
+  conf.output = { ...conf.output, publicPath: "./" };
   return conf;
-}
-
+};
