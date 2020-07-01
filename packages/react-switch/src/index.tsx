ab3c08c2a0be4cf3753c8f7ccd030f6235c57ee1
jaywcjlove <398188662@qq.com>
2020-07-01 12:28:48 +0800

    feat(Switch): Add forward Ref.

--- packages/react-switch/src/index.tsx
+++ packages/react-switch/src/index.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useImperativeHandle } from 'react';
 import { RadioAbstract, RadioAbstractProps } from '@uiw/react-radio';
 import './style/index.less';
 
 export interface SwitchProps extends RadioAbstractProps {}
 
-export default (props: SwitchProps = {}) => {
+function Switch(
+  props: SwitchProps = {},
+  ref:
+    | ((instance: HTMLInputElement) => void)
+    | React.RefObject<HTMLInputElement | null>
+    | null,
+) {
   const { prefixCls = 'w-switch', ...other } = props;
+  const inputRef = React.createRef<HTMLInputElement>();
+  useImperativeHandle(ref, () => inputRef.current);
   return (
-    <RadioAbstract prefixCls={prefixCls} {...{ ...other, type: 'checkbox' }} />
+    <RadioAbstract
+      prefixCls={prefixCls}
+      {...{ ...other, type: 'checkbox' }}
+      ref={inputRef}
+    />
   );
-};
+}
+
+export default React.forwardRef<HTMLInputElement, SwitchProps>(Switch);
