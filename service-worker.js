0a7734d5f6d26629207f6610e44e34e0e7ef587f
jaywcjlove <jaywcjlove@users.noreply.github.com>
2020-07-01 02:21:51 +0000

    deploy: 0f2e21eeed1440e7a86aa4939a6a8f92c1324a88

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8a19d23afa05a2c30bd4fd31907a2ae8.js"
+  "./precache-manifest.5763ac8714d9409876e03f1dc56f4eb1.js"
 );
 
 self.addEventListener('message', (event) => {
