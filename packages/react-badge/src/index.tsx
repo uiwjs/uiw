662fc8171394192ea210ae6d8dd2db5746382b8c
jaywcjlove <398188662@qq.com>
2020-07-03 20:31:04 +0800

    chore(Badge): Optimize the code.

--- packages/react-badge/src/index.tsx
+++ packages/react-badge/src/index.tsx
@@ -11,59 +11,49 @@ export interface BadgeProps extends IProps, HTMLSpanProps {
   count?: number;
 }
 
-export default class Badge extends React.Component<BadgeProps> {
-  public static defaultProps: BadgeProps = {
-    prefixCls: 'w-badge',
-    dot: false,
-    processing: false,
-    max: 99,
+export default (props: BadgeProps = {}) => {
+  const {
+    prefixCls = 'w-badge',
+    className,
+    style = {},
+    color,
+    max = 99,
+    dot = false,
+    processing = false,
+    count,
+    children,
+    ...other
+  } = props;
+  const supProps = {
+    className: classnames({ [`${prefixCls}-count`]: !dot, dot }),
+    style: {},
   };
-  render() {
-    const {
-      prefixCls,
-      className,
-      style,
-      color,
-      max,
-      dot,
-      processing,
-      count,
-      children,
-      ...other
-    } = this.props;
-    const supProps = {
-      className: classnames({ [`${prefixCls}-count`]: !dot, dot }),
-      style: {},
-    };
-    const warpperProps = {
-      ...other,
-      className: classnames(className, `${prefixCls}`, {
-        nowrap: !children,
-        [`${prefixCls}-status`]: !children,
-        [`${prefixCls}-processing`]: processing,
-      }),
-      style: {},
-    };
-    if (count || count === 0) {
-      supProps.style = { backgroundColor: color, ...style };
-    } else {
-      warpperProps.style = style || {};
-    }
-    return (
-      <span {...warpperProps}>
-        {color && (
-          <span
-            className={`${prefixCls}-dot`}
-            style={{ backgroundColor: color }}
-          />
-        )}
-        {children}
-        {count !== 0 && !color && (
-          <sup {...supProps}>
-            {!dot && count && max && count > max ? `${max}+` : count}
-          </sup>
-        )}
-      </span>
-    );
+  const cls = classnames(className, `${prefixCls}`, {
+    nowrap: !children,
+    [`${prefixCls}-status`]: !children,
+    [`${prefixCls}-processing`]: processing,
+  });
+
+  const warpperProps: HTMLSpanProps = {};
+  if (count || count === 0) {
+    supProps.style = { backgroundColor: color, ...style };
+  } else {
+    warpperProps.style = style || {};
   }
-}
+  return (
+    <span className={cls} {...other} {...warpperProps}>
+      {color && (
+        <span
+          className={`${prefixCls}-dot`}
+          style={{ backgroundColor: color }}
+        />
+      )}
+      {children}
+      {count !== 0 && !color && (
+        <sup {...supProps}>
+          {!dot && count && max && count > max ? `${max}+` : count}
+        </sup>
+      )}
+    </span>
+  );
+};
