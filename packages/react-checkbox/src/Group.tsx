6298fb31ae2a9e423f87e464822e80e211b2180c
jaywcjlove <398188662@qq.com>
2020-11-19 11:06:21 +0800

    type(Checkbox.Group): Fix value type errors.

--- packages/react-checkbox/src/Group.tsx
+++ packages/react-checkbox/src/Group.tsx
@@ -2,13 +2,13 @@ import React, { useRef } from 'react';
 import { IProps, HTMLDivProps } from '@uiw/utils';
 import './style/group.less';
 
-export type Value = string[] | number[];
+export type Value = string | number;
 export interface CheckboxGroupPorps
   extends IProps,
     Omit<HTMLDivProps, 'onChange'> {
-  value?: Value;
+  value?: Value[];
   name?: string;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>, values: Value) => void;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>, values: Value[]) => void;
 }
 
 export function CheckboxGroup(props: CheckboxGroupPorps = {}) {
@@ -20,12 +20,20 @@ export function CheckboxGroup(props: CheckboxGroupPorps = {}) {
     onChange,
     ...other
   } = props;
-  const valueRef = useRef<Value>([]);
+  const valueRef = useRef<Value[]>([]);
   const cls = [prefixCls, className].filter(Boolean).join(' ').trim();
+  const childs = React.Children.toArray(props.children);
   return (
     <div {...other} className={cls}>
-      {React.Children.map(props.children, (element: any) => {
-        if (Array.isArray(value) && value.includes(element.props.value)) {
+      {React.Children.map(childs, (element: React.ReactNode) => {
+        if (!React.isValidElement(element)) return;
+        if (
+          Array.isArray(value) &&
+          element &&
+          element.props &&
+          element.props.value &&
+          value.includes(element.props.value)
+        ) {
           if (!valueRef.current.includes(element.props.value)) {
             valueRef.current.push(element.props.value);
           }
@@ -44,7 +52,7 @@ export function CheckboxGroup(props: CheckboxGroupPorps = {}) {
               } else if (include && !checked) {
                 valueRef.current = valueRef.current.filter(
                   (val) => val !== element.props.value,
-                ) as Value;
+                );
               }
               onChange && onChange(e, valueRef.current);
             },
