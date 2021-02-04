d73f7d758d5154aa1c61a254fbe8ba44b744aca4
jaywcjlove <jaywcjlove@users.noreply.github.com>
2021-02-04 07:25:45 +0000

    deploy: 20671cd95f673f81b1f22f5acfb3e77982dafe1b

---
+++ static/js/vendors-remark.bf3ceb8d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@example/uiw"]=this["webpackJsonp@example/uiw"]||[]).push([[6],{1294:function(s,t,n){"use strict";s.exports=function(s){var t=this;this.Parser=function(n){return a(n,Object.assign({},t.data("settings"),s,{extensions:t.data("micromarkExtensions")||[],mdastExtensions:t.data("fromMarkdownExtensions")||[]}))}};var a=n(1295)}}]);
+//# sourceMappingURL=vendors-remark.bf3ceb8d.chunk.js.map
