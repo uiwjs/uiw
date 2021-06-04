74ce1f332a4ca57e911ef6bb2febe1ff3a2388da
jaywcjlove <398188662@qq.com>
2021-06-05 01:18:45 +0800

    chore(DateInput): Optimize the code.

--- packages/react-date-input/src/index.tsx
+++ packages/react-date-input/src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Input, { InputProps } from '@uiw/react-input';
 import Popover, { PopoverProps } from '@uiw/react-popover';
 import DatePicker, { DatePickerProps } from '@uiw/react-date-picker';
@@ -18,84 +18,63 @@ export interface IDateInputProps<T>
   onChange?: (selectedDate?: Date) => void;
 }
 
-export interface IDateInputState {
-  date?: Date | string;
-}
+export default function DateInput<T>(props: IDateInputProps<T>) {
+  const {
+    prefixCls = 'w-dateinput',
+    className,
+    popoverProps,
+    datePickerProps,
+    allowClear = true,
+    format = 'YYYY/MM/DD',
+    onChange,
+    ...inputProps
+  } = props;
+  const [date, setDate] = useState(props.value);
 
-export default class DateInput<T> extends React.Component<
-  IDateInputProps<T>,
-  IDateInputState
-> {
-  public static defaultProps: IDateInputProps<{}> = {
-    prefixCls: 'w-dateinput',
-    format: 'YYYY/MM/DD',
-    allowClear: true,
-  };
-  constructor(props: IDateInputProps<T>) {
-    super(props);
-    this.state = {
-      date: props.value,
-    };
-  }
-  UNSAFE_componentWillReceiveProps(nextProps: IDateInputProps<T>) {
-    if (nextProps.value !== this.props.value) {
-      this.setState({ date: nextProps.value });
+  const value = date || '';
+  inputProps.value =
+    typeof value === 'string' ? value : formatter(format, value);
+
+  useEffect(() => {
+    if (props.value !== date) {
+      setDate(props.value);
     }
+  }, [props.value]);
+
+  function handleChange(cdate?: Date) {
+    setDate(cdate);
+    onChange && onChange(cdate);
   }
-  onChange(date?: Date) {
-    const { format, onChange } = this.props;
-    this.setState({ date });
-    if (date) {
-      (date as any) = formatter(format as string, new Date(date));
-    }
-    onChange && onChange(date);
-  }
-  render() {
-    const {
-      prefixCls,
-      className,
-      popoverProps,
-      datePickerProps,
-      allowClear,
-      format,
-      onChange,
-      ...inputProps
-    } = this.props;
-    const { date } = this.state;
-    const value = date || '';
-    inputProps.value =
-      typeof value === 'string' ? value : formatter(format as string, value);
-    if (allowClear && inputProps.value) {
-      inputProps.addonAfter = (
-        <Icon
-          className={`${prefixCls}-close-btn`}
-          onClick={this.onChange.bind(this, undefined)}
-          type="close"
-        />
-      );
-    }
-    return (
-      <Popover
-        trigger="focus"
-        placement="bottomLeft"
-        autoAdjustOverflow
-        {...popoverProps}
-        content={
-          <DatePicker
-            date={(value && new Date(value)) || undefined}
-            className={`${prefixCls}-popover`}
-            {...datePickerProps}
-            onChange={this.onChange.bind(this) as any}
-          />
-        }
-      >
-        <Input
-          placeholder="请选择日期"
-          readOnly
-          {...(inputProps as any)}
-          className={[prefixCls, className].filter(Boolean).join(' ').trim()}
-        />
-      </Popover>
+  if (allowClear && inputProps.value) {
+    inputProps.addonAfter = (
+      <Icon
+        className={`${prefixCls}-close-btn`}
+        onClick={() => handleChange(undefined)}
+        type="close"
+      />
     );
   }
+  return (
+    <Popover
+      trigger="focus"
+      placement="bottomLeft"
+      autoAdjustOverflow
+      {...popoverProps}
+      content={
+        <DatePicker
+          date={(value && new Date(value)) || undefined}
+          className={`${prefixCls}-popover`}
+          {...datePickerProps}
+          onChange={(selectedDate) => handleChange(selectedDate!)}
+        />
+      }
+    >
+      <Input
+        placeholder="请选择日期"
+        readOnly
+        {...(inputProps as InputProps<string>)}
+        className={[prefixCls, className].filter(Boolean).join(' ').trim()}
+      />
+    </Popover>
+  );
 }
