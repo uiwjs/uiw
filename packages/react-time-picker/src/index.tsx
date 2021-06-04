6452cc5a9f36bc2c49ba15c1fbc015bb91a1961e
jaywcjlove <398188662@qq.com>
2021-06-05 01:59:36 +0800

    chore(TimePicker): Optimize the code.

--- packages/react-time-picker/src/index.tsx
+++ packages/react-time-picker/src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Popover, { PopoverProps } from '@uiw/react-popover';
 import Input, { InputProps } from '@uiw/react-input';
 import Button from '@uiw/react-button';
@@ -32,114 +32,81 @@ export interface TimePickerProps<T>
   ) => void;
 }
 
-export interface TimePickerState {
-  date: Date | '';
-}
-
-export default class TimePicker<T> extends React.Component<
-  TimePickerProps<T>,
-  TimePickerState
-> {
-  public state: TimePickerState;
-  public static defaultProps: TimePickerProps<{}> = {
-    prefixCls: 'w-timepicker',
-    format: 'HH:mm:ss',
-    allowClear: true,
+export default function TimePicker<T>(props: TimePickerProps<T>) {
+  const {
+    prefixCls = 'w-timepicker',
+    className,
+    disabled,
+    value,
+    format = 'HH:mm:ss',
+    popoverProps,
+    allowClear = true,
+    onChange,
+    disabledHours,
+    disabledMinutes,
+    disabledSeconds,
+    hideDisabled,
+    precision,
+    ...inputProps
+  } = props;
+  const [date, setDate] = useState(props.value);
+  useEffect(() => setDate(props.value), [props.value]);
+  const timeProps = {
+    disabledHours,
+    disabledMinutes,
+    disabledSeconds,
+    hideDisabled,
+    precision,
   };
-  constructor(props: TimePickerProps<T>) {
-    super(props);
-    this.state = {
-      date: (props.value || '') as Date,
-    };
-  }
-  UNSAFE_componentWillReceiveProps(nextProps: TimePickerProps<T>) {
-    if (nextProps.value !== this.props.value) {
-      this.setState({ date: nextProps.value as Date });
-    }
-  }
-  onClear = () => {
-    const { onChange } = this.props;
-    this.setState({ date: '' }, () => onChange && onChange());
-  };
-  onSelected(
-    type: TimePickerTimeProps['type'],
-    num?: number,
-    disableds?: number[],
-    date?: TimePickerState['date'],
-  ) {
-    const { onChange, format } = this.props;
-    this.setState({ date: date || '' });
-    const dataStr = date ? formatter(format!, date) : '';
-    onChange && onChange(dataStr, date, type, num, disableds);
-  }
-  render() {
-    const {
-      prefixCls,
-      className,
-      disabled,
-      value,
-      format,
-      popoverProps,
-      allowClear,
-      onChange,
-      disabledHours,
-      disabledMinutes,
-      disabledSeconds,
-      hideDisabled,
-      precision,
-      ...inputProps
-    } = this.props;
-    const { date } = this.state;
-    const timeProps = {
-      disabledHours,
-      disabledMinutes,
-      disabledSeconds,
-      hideDisabled,
-      precision,
-    };
-    const inputValue = date ? formatter(format as string, date) : '';
-    const props: InputProps<T> = { ...inputProps, value: inputValue };
-    const datePickerTime = date || new Date();
-    if (allowClear && inputValue !== '' && !!inputValue) {
-      props.addonAfter = (
-        <Button
-          className={`${prefixCls}-close-btn`}
-          icon="close"
-          disabled={props.disabled}
-          onClick={this.onClear}
-          size={props.size}
-          basic
-          type="light"
-        />
-      );
-    }
-    return (
-      <Popover
-        trigger="focus"
-        placement="bottomLeft"
-        autoAdjustOverflow
-        visibleArrow={false}
-        {...popoverProps}
-        content={
-          <TimePickerTime
-            className={`${prefixCls}-popover`}
-            {...timeProps}
-            date={datePickerTime}
-            onSelected={this.onSelected.bind(this)}
-          />
-        }
-      >
-        <Input
-          placeholder="请选择时间"
-          readOnly
-          disabled={disabled}
-          {...props}
-          className={[`${prefixCls}-input`, className]
-            .filter(Boolean)
-            .join(' ')
-            .trim()}
-        />
-      </Popover>
+  const inputValue = date ? formatter(format, date) : '';
+  const datePickerTime = date || new Date();
+  const _props: InputProps<T> = { ...inputProps, value: inputValue };
+  if (allowClear && inputValue !== '' && !!inputValue) {
+    _props.addonAfter = (
+      <Button
+        className={`${prefixCls}-close-btn`}
+        icon="close"
+        disabled={props.disabled}
+        onClick={() => {
+          setDate(undefined);
+          onChange && onChange();
+        }}
+        size={props.size}
+        basic
+        type="light"
+      />
     );
   }
+  return (
+    <Popover
+      trigger="focus"
+      placement="bottomLeft"
+      autoAdjustOverflow
+      visibleArrow={false}
+      {...popoverProps}
+      content={
+        <TimePickerTime
+          className={`${prefixCls}-popover`}
+          {...timeProps}
+          date={datePickerTime}
+          onSelected={(type, num, disableds, date) => {
+            setDate(date);
+            const dataStr = date ? formatter(format, date) : '';
+            onChange && onChange(dataStr, date, type, num, disableds);
+          }}
+        />
+      }
+    >
+      <Input
+        placeholder="请选择时间"
+        readOnly
+        disabled={disabled}
+        {..._props}
+        className={[`${prefixCls}-input`, className]
+          .filter(Boolean)
+          .join(' ')
+          .trim()}
+      />
+    </Popover>
+  );
 }
