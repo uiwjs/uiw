08b5302df1a7d08e3ffa39f76c43f08b361ea9e7
jaywcjlove <398188662@qq.com>
2021-06-17 09:28:20 +0800

    feat(Input): Using ref forwarding.

--- packages/react-input/src/index.tsx
+++ packages/react-input/src/index.tsx
@@ -9,68 +9,63 @@ export interface InputProps<T> extends IProps, Omit<HTMLInputProps, 'size'> {
   size?: 'large' | 'default' | 'small';
 }
 
-function InternalInput<T>(
-  props: InputProps<T> = {},
-  ref:
-    | ((instance: HTMLInputElement) => void)
-    | React.RefObject<HTMLInputElement | null>
-    | null,
-) {
-  const {
-    prefixCls = 'w-input',
-    className,
-    style,
-    size = 'default',
-    type = 'text',
-    preIcon = null,
-    addonAfter,
-    ...otherProps
-  } = props;
-  const inputRef = React.createRef<HTMLInputElement>();
-  const addonRef = React.createRef<HTMLSpanElement>();
-  useImperativeHandle(ref, () => inputRef.current);
-  const cls = [
-    prefixCls,
-    className,
-    size ? `${prefixCls}-${size}` : null,
-    addonAfter ? `${prefixCls}-addon` : null,
-    props.disabled ? 'disabled' : null,
-  ]
-    .filter(Boolean)
-    .join(' ')
-    .trim();
-
-  useEffect(() => {
-    computedInputPadding();
-  });
-
-  function computedInputPadding() {
-    if (addonRef.current && inputRef.current) {
-      const input = window && window.getComputedStyle(addonRef.current, null);
-      inputRef.current.style.paddingRight = `${
-        addonRef.current.clientWidth + parseInt(input.right as string, 10) * 2
-      }px`;
-    }
-  }
-  return (
-    <div className={cls} style={style}>
-      <Icon type={preIcon} />
-      <input
-        ref={inputRef}
-        type={type}
-        autoComplete="off"
-        {...otherProps}
-        className={`${prefixCls}-inner`}
-      />
-      {addonAfter && (
-        <span className={`${prefixCls}-addon-after`} ref={addonRef}>
-          {addonAfter}
-        </span>
-      )}
-    </div>
-  );
-}
-
 export default React.forwardRef<HTMLInputElement, InputProps<{}>>(
-  InternalInput,
+  (props, ref) => {
+    const {
+      prefixCls = 'w-input',
+      className,
+      style,
+      size = 'default',
+      type = 'text',
+      preIcon = null,
+      addonAfter,
+      ...otherProps
+    } = props;
+    const inputRef = React.useRef<HTMLInputElement>(null);
+    const addonRef = React.useRef<HTMLSpanElement>(null);
+    useImperativeHandle<HTMLInputElement | null, HTMLInputElement | null>(
+      ref,
+      () => inputRef.current,
+    );
+    const cls = [
+      prefixCls,
+      className,
+      size ? `${prefixCls}-${size}` : null,
+      addonAfter ? `${prefixCls}-addon` : null,
+      props.disabled ? 'disabled' : null,
+    ]
+      .filter(Boolean)
+      .join(' ')
+      .trim();
+
+    useEffect(() => {
+      computedInputPadding();
+    });
+
+    function computedInputPadding() {
+      if (addonRef.current && inputRef.current) {
+        const input = window && window.getComputedStyle(addonRef.current, null);
+        inputRef.current.style.paddingRight = `${
+          addonRef.current.clientWidth + parseInt(input.right as string, 10) * 2
+        }px`;
+      }
+    }
+    return (
+      <div className={cls} style={style}>
+        <Icon type={preIcon} />
+        <input
+          ref={inputRef}
+          type={type}
+          autoComplete="off"
+          {...otherProps}
+          className={`${prefixCls}-inner`}
+        />
+        {addonAfter && (
+          <span className={`${prefixCls}-addon-after`} ref={addonRef}>
+            {addonAfter}
+          </span>
+        )}
+      </div>
+    );
+  },
 );
