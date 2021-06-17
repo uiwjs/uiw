59e39fd90e0de8f4548df78fedfb8d09aab9e5d7
jaywcjlove <398188662@qq.com>
2021-06-17 09:34:27 +0800

    feat(FileInput): Using ref forwarding.

--- packages/react-file-input/src/index.tsx
+++ packages/react-file-input/src/index.tsx
@@ -1,23 +1,15 @@
-import React, { useImperativeHandle } from 'react';
+import React from 'react';
 import Input, { InputProps } from '@uiw/react-input';
 import './style/index.less';
 
 export interface FileInputProps<T> extends InputProps<T> {}
 
-function FileInput<T>(
-  { ...props }: FileInputProps<T>,
-  ref:
-    | ((instance: HTMLInputElement) => void)
-    | React.RefObject<HTMLInputElement | null>
-    | null,
-): JSX.Element {
-  const inputRef = React.createRef<HTMLInputElement>();
-  useImperativeHandle(ref, () => inputRef.current);
-  props.className = [props.className, 'w-fileinput']
-    .filter(Boolean)
-    .join(' ')
-    .trim();
-  return <Input ref={inputRef} data-label="Browse" {...props} type="file" />;
-}
-
-export default React.forwardRef<HTMLInputElement, InputProps<{}>>(FileInput);
+export default React.forwardRef<HTMLInputElement, InputProps<{}>>(
+  (props, ref) => {
+    props.className = [props.className, 'w-fileinput']
+      .filter(Boolean)
+      .join(' ')
+      .trim();
+    return <Input ref={ref} data-label="Browse" {...props} type="file" />;
+  },
+);
