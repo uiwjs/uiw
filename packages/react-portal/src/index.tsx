0ae75a4e8766bea95524ec62bbe681e99b6a7994
jaywcjlove <398188662@qq.com>
2021-06-21 15:55:54 +0800

    refactor(Portal): Refactor Portal componemt.

--- packages/react-portal/src/index.tsx
+++ packages/react-portal/src/index.tsx
@@ -1,73 +1,51 @@
-import React, { Fragment, useEffect, useState, useMemo } from 'react';
+import React, { useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
+
+export const canUseDOM = !!(
+  typeof window !== 'undefined' &&
+  window.document &&
+  window.document.createElement
+);
 
 export interface PortalProps {
   children?: React.ReactNode;
-  /**
-   * Callback invoked when the children of this `Portal` have been added to the DOM.
-   */
-  onChildrenMount?: (portalElement: HTMLElement) => void;
   /**
    * The HTML element that children will be mounted to.
    * @default document.body
    */
   container?: HTMLElement;
+  /**
+   * Callback invoked when the children of this `Portal` have been added to the DOM.
+   * @deprecated v4.9.0+
+   */
+  onChildrenMount?: (portalElement: HTMLElement) => void;
+  /** @deprecated v4.9.0+ */
   visible?: boolean;
 }
 
-export interface PortalState {
-  hasMounted: boolean;
-}
+export default function Portal(props: PortalProps) {
+  const { container } = props;
+  const defaultNode = useRef<HTMLDivElement>();
+  const containerRef = useRef<HTMLElement | undefined>(container);
 
-export default function Portal(props: PortalProps = {}) {
-  const {
-    children,
-    container: body = typeof document !== 'undefined'
-      ? document.body
-      : undefined,
-    visible,
-    onChildrenMount,
-  } = props;
-  const [hasMounted, setHasMounted] = useState(false);
-  const [portalElement, setPortalElement] = useState<HTMLDivElement>();
   useEffect(() => {
-    // Component Will Unmount
     return () => {
-      if (portalElement) {
-        portalElement.remove();
+      if (defaultNode.current && containerRef.current) {
+        containerRef.current.removeChild(defaultNode.current);
+        defaultNode.current = undefined;
       }
     };
   }, []);
-  useMemo(() => {
-    if (visible && body) {
-      if (!portalElement) {
-        let elm = document.createElement('div');
-        setPortalElement(elm);
-        body.appendChild(elm);
-      }
-    }
-    if (!visible && portalElement) {
-      setHasMounted(false);
-      setPortalElement(undefined);
-      const timer = setTimeout(() => {
-        portalElement && portalElement.remove();
-        clearTimeout(timer);
-      });
-    }
-  }, [visible]);
-  useMemo(() => {
-    if (portalElement && !hasMounted) {
-      setHasMounted(true);
-      onChildrenMount && onChildrenMount(portalElement);
-    }
-  }, [portalElement, hasMounted]);
 
-  // Only render `children` once this component has mounted in a browser environment,
-  // so they are immediately attached to the DOM tree.
-  // See long comment on componentDidMount in https://reactjs.org/docs/portals.html#event-bubbling-through-portals
-  if (typeof document === 'undefined' || !hasMounted) {
-    return <Fragment />;
-  } else {
-    return ReactDOM.createPortal(children, portalElement!);
+  if (!canUseDOM) {
+    return null;
   }
+  if (!containerRef.current) {
+    containerRef.current = document.body;
+  }
+  if (!defaultNode.current) {
+    defaultNode.current = document.createElement('div');
+    containerRef.current.appendChild(defaultNode.current);
+  }
+  return ReactDOM.createPortal(props.children, defaultNode.current);
 }
