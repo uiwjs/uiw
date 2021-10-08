699405b5a3a358f24510d61524ba3ce9198c491a
jaywcjlove <398188662@qq.com>
2021-10-08 14:04:50 +0800

    website: Update edit ulr error.

--- website/src/routes/components/colors/index.tsx
+++ website/src/routes/components/colors/index.tsx
@@ -4,7 +4,7 @@ import Markdown from '../../../components/Markdown';
 export default function Page() {
   return (
     <Markdown
-      path="https://github.com/uiwjs/uiw/tree/master/website/uiw/src/routes/components/colors/README.md"
+      path="https://github.com/uiwjs/uiw/tree/master/website/src/routes/components/colors/README.md"
       renderPage={async () => {
         const md = await import('./README.md');
         return md.default || md;
--- website/src/routes/guide/docs/index.tsx
+++ website/src/routes/guide/docs/index.tsx
@@ -3,7 +3,7 @@ import Markdown from '../../../components/Markdown';
 
 export default () => (
   <Markdown
-    path="https://github.com/uiwjs/uiw/tree/master/website/uiw/src/routes/guide/docs/README.md"
+    path="https://github.com/uiwjs/uiw/tree/master/website/src/routes/guide/docs/README.md"
     renderPage={async () => {
       const md = await import('./README.md');
       return md.default || md;
