285a561398f3568811e61d41b1f1eeeaa26ebadd
jaywcjlove <398188662@qq.com>
2022-01-24 19:06:02 +0800

    type(FileInput): Fix type errors.

--- packages/react-file-input/src/index.tsx
+++ packages/react-file-input/src/index.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import Input, { InputProps } from '@uiw/react-input';
 import './style/index.less';
 
-export interface FileInputProps<T> extends InputProps<T> {}
+export interface FileInputProps extends InputProps {}
 
-export default React.forwardRef<HTMLInputElement, InputProps<{}>>((props, ref) => {
+export default React.forwardRef<HTMLInputElement, InputProps>((props, ref) => {
   const { className, prefixCls = 'w-fileinput', ...other } = props;
   const cls = [prefixCls, className].filter(Boolean).join(' ').trim();
   return <Input ref={ref} data-label="Browse" className={cls} {...other} type="file" />;
