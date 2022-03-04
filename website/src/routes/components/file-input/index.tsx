062a3ded2d91093460869b7c44b13b1409c961a6
zhouzheng <zhouzheng0646@qq.com>
2022-03-04 12:00:44 +0800

    docs(FileInput): 增加图片预览示例

--- website/src/routes/components/file-input/index.tsx
+++ website/src/routes/components/file-input/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { FileInput, Button, Icon, Form, Row, Col } from 'uiw';
+import { FileInput, Overlay, Button, Icon, Form, Row, Col } from 'uiw';
 import Markdown from '../../../components/Markdown';
 
 export default function Page() {
   return (
     <Markdown
       path="https://github.com/uiwjs/uiw/tree/master/packages/react-file-input/README.md"
-      dependencies={{ FileInput, Button, Icon, Form, Row, Col }}
+      dependencies={{ FileInput, Button, Overlay, Icon, Form, Row, Col }}
       renderPage={async () => {
         const md = await import('uiw/node_modules/@uiw/react-file-input/README.md');
         return md.default || md;
