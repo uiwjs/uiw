b0f0226c79c624581d6adc0f997504d792a57011
zhouzheng <zhouzheng0646@qq.com>
2022-03-08 09:41:46 +0800

    feat(Tabs): 选项卡过多,超过宽度时,收缩超出部分

--- packages/react-tabs/src/index.tsx
+++ packages/react-tabs/src/index.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, useCallback, useMemo } from 'react';
 import { IProps, HTMLDivProps } from '@uiw/utils';
 import Pane from './Pane';
 import './style/index.less';
+import Popover from '@uiw/react-popover';
 
 export * from './Pane';
 
@@ -15,6 +16,14 @@ export interface TabsProps extends IProps, HTMLDivProps {
   onTabClick?: (key: string, item: React.ReactElement, e: React.MouseEvent) => void;
 }
 
+type flowNavType = {
+  content: number;
+  nav: Array<Record<'index' | 'width' | 'curWidth', number>>;
+  flowLeft: number;
+  displayStart: number;
+  displayEnd: number;
+};
+
 export default function Tabs(props: TabsProps) {
   const {
     prefixCls = 'w-tabs',
@@ -25,10 +34,69 @@ export default function Tabs(props: TabsProps) {
     onTabClick,
     ...elementProps
   } = props;
+
   const [activeKey, setActiveKey] = useState(props.activeKey);
   const [slideStyle, setSlideStyle] = useState({ width: 0, left: 0 });
   const activeItem = useRef<HTMLDivElement | undefined>();
   const cls = [prefixCls, className, type ? `${prefixCls}-${type}` : null].filter(Boolean).join(' ').trim();
+
+  const [flowNav, flowNavSet] = useState<flowNavType>({
+    content: 0,
+    nav: [],
+    flowLeft: -1,
+    displayStart: 0,
+    displayEnd: 0,
+  });
+  const [hiddenNav, hiddenNavSet] = useState<Array<number>>([]);
+  const deviation = 15;
+
+  const [nodes, nodesSet] = useState<any>();
+  const divContentRef = useCallback((node) => {
+    if (node !== null) {
+      nodesSet(nodes);
+      node.addEventListener('scroll', (e: any) => {
+        const { clientWidth, scrollLeft } = e.target;
+        flowNav.displayStart = scrollLeft;
+        flowNav.displayEnd = clientWidth + scrollLeft;
+        flowNavSet({ ...flowNav });
+      });
+      flowNav.displayEnd = node.getBoundingClientRect().width;
+      flowNavSet({ ...flowNav });
+    }
+  }, []);
+
+  const divNavRef = useCallback((node, key: number) => {
+    if (node !== null) {
+      node.addEventListener('click', (e: any) => {
+        activeItem.current = node;
+      });
+      divNavWidthChange(node.getBoundingClientRect().width, key);
+    }
+  }, []);
+
+  const divNavWidthChange = (width: number, index: number) => {
+    let curWidth = 0;
+    flowNav.nav.slice(0, index + 1).forEach((nav) => (curWidth += nav.width));
+    flowNav.nav[index] = { width, curWidth: Math.floor(curWidth), index };
+    flowNavSet(flowNav);
+  };
+
+  useEffect(() => {
+    showHideenNav();
+  }, [flowNav.displayEnd > flowNav.nav[flowNav.nav.length - 1]?.curWidth]);
+
+  const showHideenNav = () => {
+    const hiddenNav: Array<number> = [];
+    if (flowNav.nav.length > 0) {
+      flowNav.nav.forEach((item) => {
+        const curWidth = item.curWidth - deviation;
+        if (curWidth < flowNav.displayStart || curWidth > flowNav.displayEnd) {
+          hiddenNav.push(item.index);
+        }
+      });
+      hiddenNavSet(hiddenNav);
+    }
+  };
 
   useEffect(() => setActiveKey(props.activeKey), [props.activeKey]);
   useEffect(() => calcSlideStyle(), [activeKey]);
@@ -44,44 +112,29 @@ export default function Tabs(props: TabsProps) {
 
   return (
     <div className={cls} {...elementProps}>
-      <div className={`${prefixCls}-bar`}>
-        <div className={`${prefixCls}-nav`}>
-          {React.Children.map(children as React.ReactElement[], (item: React.ReactElement, key: number) => {
-            if (!item) {
-              return null;
-            }
-            const divProps: HTMLDivProps = {
-              className: [
-                `${prefixCls}-item`,
-                item.key === activeKey ? 'active' : null,
-                item.props.disabled ? 'disabled' : null,
-              ]
-                .filter(Boolean)
-                .join(' ')
-                .trim(),
-              children: item.props.label,
-            };
-            if (!item.props.disabled) {
-              divProps.onClick = (e: React.MouseEvent) => {
-                setActiveKey(item.key as string);
-                onTabClick && onTabClick(item.key as string, item, e);
-                calcSlideStyle();
-              };
-            }
-            return (
-              <div
-                key={key}
-                ref={(node) => {
-                  if (node && item.key === activeKey) {
-                    activeItem.current = node;
-                  }
-                }}
-                {...divProps}
-              />
-            );
-          })}
+      <div style={{ display: 'flex' }}>
+        <div className={`${prefixCls}-bar`} ref={divContentRef}>
+          <div className={`${prefixCls}-nav`} style={{ width: 'max-content' }}>
+            {renderNav(children)}
+          </div>
         </div>
         <div style={slideStyle} className={`${prefixCls}-slide`} />
+        {hiddenNav.length > 0 && (
+          <Popover
+            trigger="click"
+            placement="bottomRight"
+            visibleArrow={false}
+            content={
+              <div className={`${prefixCls}-nav-hidden`}>
+                {renderNav(hiddenNav.map((idx) => (children as Array<React.ReactElement>)[idx]))}
+              </div>
+            }
+          >
+            <div onClick={showHideenNav} className={`${prefixCls}-flow-content`}>
+              <span>…</span>
+            </div>
+          </Popover>
+        )}
       </div>
       {React.Children.map(children, (item: any) => {
         if (!item || activeKey !== item.key) {
@@ -91,4 +144,31 @@ export default function Tabs(props: TabsProps) {
       })}
     </div>
   );
+
+  function renderNav(children: React.ReactNode): React.ReactNode {
+    return React.Children.map(children as React.ReactElement[], (item: React.ReactElement, key: number) => {
+      if (!item) {
+        return null;
+      }
+      const divProps: HTMLDivProps = {
+        className: [
+          `${prefixCls}-item`,
+          item.key === activeKey ? 'active' : null,
+          item.props.disabled ? 'disabled' : null,
+        ]
+          .filter(Boolean)
+          .join(' ')
+          .trim(),
+        children: item.props.label,
+      };
+      if (!item.props.disabled) {
+        divProps.onClick = (e: React.MouseEvent) => {
+          setActiveKey(item.key as string);
+          onTabClick && onTabClick(item.key as string, item, e);
+          calcSlideStyle();
+        };
+      }
+      return <div key={key} ref={(ref) => divNavRef(ref, key)} {...divProps} />;
+    });
+  }
 }
