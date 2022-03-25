d6757eeed9296ebbd8e89ccdeff71ee29eb07470
WmW <48251883+nullptr-z@users.noreply.github.com>
2022-03-25 13:29:15 +0800

    feat(Cascader): 添加表单支持 & 增加清除按钮 (#713)

--- packages/react-cascader/src/index.tsx
+++ packages/react-cascader/src/index.tsx
@@ -4,45 +4,85 @@ import { IProps } from '@uiw/utils';
 import Dropdown, { DropdownProps } from '@uiw/react-dropdown';
 import Menu from '@uiw/react-menu';
 import Icon from '@uiw/react-icon';
+import './style/index.less';
 
 type ValueType = Array<string | number>;
-type optionType = { value: string | number; label: React.ReactNode; children?: Array<optionType> };
+type OptionType = { value: string | number; label: React.ReactNode; children?: Array<OptionType> };
 
 export interface CascaderProps extends IProps, DropdownProps {
-  option?: Array<optionType>;
+  option?: Array<OptionType>;
   value?: ValueType;
-  onChange?: (value: ValueType, selectedOptions: Array<optionType>) => void;
+  onChange?: (isSeleted: boolean, value: ValueType, selectedOptions: Array<OptionType>) => void;
   allowClear?: boolean;
   placeholder?: string;
   isOpen?: boolean;
 }
 
 function Cascader(props: CascaderProps) {
-  const { placeholder, prefixCls = 'w-search-select', className, style = { width: 200 }, option = [], others } = props;
+  const {
+    value,
+    onChange,
+
+    allowClear,
+    placeholder,
+    prefixCls = 'w-cascader',
+    className,
+    style = { width: 200 },
+    option = [],
+    others,
+  } = props;
 
   const cls = [prefixCls, className].filter(Boolean).join(' ').trim();
   const [innerIsOpen, setInnerIsOpen] = useState(false);
-  const [selectedValue, setSelectedValue] = useState<Array<optionType>>([]);
+  const [selectedValue, setSelectedValue] = useState<Array<OptionType>>([]);
+  const [selectIconType, setSelectIconType] = useState('');
+
+  useEffect(() => {
+    const valueTemp: Array<OptionType> = [];
+    let optChildren = option;
+    value?.map((item) => {
+      const findOpt = optChildren.find((opt) => opt.value === item);
+      optChildren = findOpt?.children || [];
+      valueTemp.push({ label: item, value: item, ...findOpt });
+    });
+    setSelectedValue(valueTemp);
+  }, [value]);
 
   function onVisibleChange(isOpen: boolean) {
     setInnerIsOpen(isOpen);
   }
 
-  const handleItemClick = (optionsItem: optionType, level: number) => {
+  function renderSelectIcon(type: string) {
+    let selectIconType;
+    if (type === 'enter' && allowClear && selectedValue.length > 0) {
+      selectIconType = 'close';
+    } else {
+      selectIconType = '';
+    }
+    setSelectIconType(selectIconType);
+  }
+
+  const handleItemClick = (optionsItem: OptionType, level: number) => {
     selectedValue.splice(level, selectedValue.length - level, optionsItem);
+
+    handelChange(true, selectedValue);
+  };
+
+  const handelChange = (isSeleted: boolean, selectedValue: Array<OptionType>) => {
     setSelectedValue([...selectedValue]);
-
-    handelChange();
-  };
-
-  const handelChange = () => {
     const value = selectedValue.map((item) => item.value);
-    props.onChange?.(value, selectedValue);
+    onChange?.(isSeleted, value, selectedValue);
   };
 
-  const widths = (style?.width as number) * 0.6 || undefined;
+  const onClear = (e: React.MouseEvent<any, MouseEvent>) => {
+    e.stopPropagation();
+    console.log(123);
+    handelChange(false, []);
+  };
 
-  const OptionIter = (option: Array<optionType>, level: number = 0) => {
+  const widths = (style?.width as number) * 0.5 || undefined;
+
+  const OptionIter = (option: Array<OptionType>, level: number = 0) => {
     if (!option) return;
 
     return (
@@ -59,15 +99,15 @@ function Cascader(props: CascaderProps) {
         {!option || option.length === 0 ? (
           <div style={{ color: '#c7c7c7', fontSize: 12 }}>{'没有数据'}</div>
         ) : (
-          option.map((item, index) => {
-            const active = selectedValue[level]?.value === item.value;
+          option.map((opt, index) => {
+            const active = selectedValue[level]?.value === opt.value;
             return (
               <Menu.Item
                 active={active}
                 key={index}
-                text={item.label}
-                addonAfter={item.children ? <Icon type="right" /> : undefined}
-                onClick={() => handleItemClick(item, level)}
+                text={opt.label}
+                addonAfter={opt.children ? <Icon type="right" /> : undefined}
+                onClick={() => handleItemClick(opt, level)}
               />
             );
           })
@@ -77,7 +117,7 @@ function Cascader(props: CascaderProps) {
   };
 
   const inputValue = useMemo(() => {
-    return selectedValue.map((item) => item.label).join(' / ');
+    return selectedValue.map((opt) => opt.label).join(' / ');
   }, [selectedValue.length]);
 
   return (
@@ -101,7 +141,20 @@ function Cascader(props: CascaderProps) {
         </div>
       }
     >
-      <Input value={inputValue} onChange={() => {}} placeholder={placeholder} style={{ width: style?.width }} />
+      <span onMouseLeave={() => renderSelectIcon('leave')} onMouseOver={() => renderSelectIcon('enter')}>
+        <Input
+          value={inputValue}
+          onChange={() => {}}
+          placeholder={placeholder}
+          style={{ width: style?.width }}
+          readOnly
+          addonAfter={
+            selectIconType === 'close' && (
+              <Icon type={`${selectIconType}`} onClick={onClear} className={`${prefixCls}-close`} />
+            )
+          }
+        />
+      </span>
     </Dropdown>
   );
 }
