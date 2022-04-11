fc48558db87cd73a11422d5252c0c47fda61f1eb
zhouzheng <zhouzheng0646@qq.com>
2022-04-11 15:41:16 +0800

    clean(Tree): 删除StrictMode

--- packages/react-tree/src/TreeNode.tsx
+++ packages/react-tree/src/TreeNode.tsx
@@ -72,115 +72,113 @@ export default function TreeNode<T>(props: TreeNodeProps<T>) {
   }, []);
 
   return (
-    <React.StrictMode>
-      <CSSTransition
-        nodeRef={node}
-        classNames={prefixCls}
-        in={isOpen}
-        timeout={200}
-        onExit={onExit}
-        onExiting={onExiting}
-        onEnter={onEnter}
-        onEntered={onEntered}
-        onEntering={onEntering}
+    <CSSTransition
+      nodeRef={node}
+      classNames={prefixCls}
+      in={isOpen}
+      timeout={200}
+      onExit={onExit}
+      onExiting={onExiting}
+      onEnter={onEnter}
+      onEntered={onEntered}
+      onEntering={onEntering}
+    >
+      <ul
+        ref={node}
+        className={[
+          level !== 1 && isOpen ? [`${prefixCls}-open`] : null,
+          level !== 1 && !isOpen ? [`${prefixCls}-close`] : null,
+        ]
+          .filter(Boolean)
+          .join(' ')
+          .trim()}
       >
-        <ul
-          ref={node}
-          className={[
-            level !== 1 && isOpen ? [`${prefixCls}-open`] : null,
-            level !== 1 && !isOpen ? [`${prefixCls}-close`] : null,
-          ]
-            .filter(Boolean)
-            .join(' ')
-            .trim()}
-        >
-          {data.map((item, idx: number) => {
-            item.parent = parent;
-            const selected = !!(selectedKeys && selectedKeys.indexOf(item.key) > -1);
-            const noChild = !item.children;
-            const itemIsOpen = openKeys && openKeys.indexOf(item.key) > -1 && !!item.children;
-            const iconItem =
-              typeof icon === 'function'
-                ? icon(item, {
-                    isOpen: !!itemIsOpen,
-                    noChild,
-                    openKeys,
-                    selectedKeys,
-                  })
-                : icon;
-            const childKeys = noChild ? [] : getChildKeys(item.children);
-            const checkedKeys = selectedKeys ? selectedKeys.filter((key) => childKeys.indexOf(key) > -1) : [];
-            const isHalfChecked = checkedKeys.length > 0 && childKeys.length !== checkedKeys.length;
-            return (
-              <li key={idx} style={{ display: item.hideNode ? 'none' : 'block' }}>
-                <div className={`${prefixCls}-label`}>
-                  <span
-                    style={{ display: noChild ? 'none' : 'auto' }}
-                    className={`${prefixCls}-switcher`}
-                    onClick={(evn) => onItemClick(item, evn)}
-                  >
-                    <Icon
-                      type={iconItem || 'caret-right'}
-                      className={[
-                        typeof icon === 'function' ? `${prefixCls}-switcher-noop` : null,
-                        noChild ? 'no-child' : null,
-                        !iconAnimation ? 'no-animation' : null,
-                        itemIsOpen ? 'open' : null,
-                      ]
-                        .filter(Boolean)
-                        .join(' ')
-                        .trim()}
-                    />
-                  </span>
-                  <div
-                    onClick={(evn) => onItemSelected(item, evn)}
+        {data.map((item, idx: number) => {
+          item.parent = parent;
+          const selected = !!(selectedKeys && selectedKeys.indexOf(item.key) > -1);
+          const noChild = !item.children;
+          const itemIsOpen = openKeys && openKeys.indexOf(item.key) > -1 && !!item.children;
+          const iconItem =
+            typeof icon === 'function'
+              ? icon(item, {
+                  isOpen: !!itemIsOpen,
+                  noChild,
+                  openKeys,
+                  selectedKeys,
+                })
+              : icon;
+          const childKeys = noChild ? [] : getChildKeys(item.children);
+          const checkedKeys = selectedKeys ? selectedKeys.filter((key) => childKeys.indexOf(key) > -1) : [];
+          const isHalfChecked = checkedKeys.length > 0 && childKeys.length !== checkedKeys.length;
+          return (
+            <li key={idx} style={{ display: item.hideNode ? 'none' : 'block' }}>
+              <div className={`${prefixCls}-label`}>
+                <span
+                  style={{ display: noChild ? 'none' : 'auto' }}
+                  className={`${prefixCls}-switcher`}
+                  onClick={(evn) => onItemClick(item, evn)}
+                >
+                  <Icon
+                    type={iconItem || 'caret-right'}
                     className={[
-                      `${prefixCls}-title`,
-                      selected && isSelected ? 'selected' : null,
-                      item.disabled ? 'disabled' : null,
+                      typeof icon === 'function' ? `${prefixCls}-switcher-noop` : null,
+                      noChild ? 'no-child' : null,
+                      !iconAnimation ? 'no-animation' : null,
+                      itemIsOpen ? 'open' : null,
                     ]
                       .filter(Boolean)
                       .join(' ')
                       .trim()}
-                  >
-                    {renderTitle ? (
-                      renderTitle(item, {
-                        selected,
-                        noChild,
-                        openKeys,
-                        isHalfChecked,
-                        selectedKeys,
-                      })
-                    ) : (
-                      <Label label={item.label} />
-                    )}
-                  </div>
-                </div>
-                {item.children && (
-                  <TreeNode
-                    {...other}
-                    {...{
-                      prefixCls,
-                      icon,
-                      iconAnimation,
-                      isSelected,
-                      openKeys,
-                      selectedKeys,
-                      onItemClick,
-                      onItemSelected,
-                      renderTitle,
-                    }}
-                    prefixCls={prefixCls}
-                    data={item.children}
-                    level={level + 1}
-                    parent={item}
                   />
-                )}
-              </li>
-            );
-          })}
-        </ul>
-      </CSSTransition>
-    </React.StrictMode>
+                </span>
+                <div
+                  onClick={(evn) => onItemSelected(item, evn)}
+                  className={[
+                    `${prefixCls}-title`,
+                    selected && isSelected ? 'selected' : null,
+                    item.disabled ? 'disabled' : null,
+                  ]
+                    .filter(Boolean)
+                    .join(' ')
+                    .trim()}
+                >
+                  {renderTitle ? (
+                    renderTitle(item, {
+                      selected,
+                      noChild,
+                      openKeys,
+                      isHalfChecked,
+                      selectedKeys,
+                    })
+                  ) : (
+                    <Label label={item.label} />
+                  )}
+                </div>
+              </div>
+              {item.children && (
+                <TreeNode
+                  {...other}
+                  {...{
+                    prefixCls,
+                    icon,
+                    iconAnimation,
+                    isSelected,
+                    openKeys,
+                    selectedKeys,
+                    onItemClick,
+                    onItemSelected,
+                    renderTitle,
+                  }}
+                  prefixCls={prefixCls}
+                  data={item.children}
+                  level={level + 1}
+                  parent={item}
+                />
+              )}
+            </li>
+          );
+        })}
+      </ul>
+    </CSSTransition>
   );
 }
