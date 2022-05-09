1a77d6ae0360a792ec36d8b9d31e4322c54020a0
SunLxy <1011771396@qq.com>
2022-05-09 09:36:11 +0800

    fix:修改国际化加载markdown文档

--- website/src/components/useMdData/index.ts
+++ website/src/components/useMdData/index.ts
@@ -1,24 +1,29 @@
 import { useEffect, useState } from 'react';
 import { CodeBlockData } from 'markdown-react-code-preview-loader';
 import { useTranslation } from 'react-i18next';
+import { DefLan } from 'react-i18next-config';
 
-const useMdData = (path?: (lang?: string) => Promise<CodeBlockData>, name: string = 'md-language-name') => {
-  const init = useTranslation();
+export const useFileSuffix = () => {
+  const { i18n } = useTranslation();
+  const fileSuffix = i18n.language === DefLan ? '' : '.' + i18n.language;
+  return fileSuffix;
+};
+
+const useMdData = (path?: (lang?: string) => Promise<CodeBlockData>) => {
   const [mdData, setMdData] = useState<CodeBlockData>({
     source: '',
     components: {},
     codeBlock: {},
     languages: {},
   });
-  const lang = init.t(name);
-
+  const fileSuffix = useFileSuffix();
   const [loading, setLoading] = useState(false);
   useEffect(() => {
     const getMd = async () => {
       if (path) {
         setLoading(() => true);
         try {
-          const result = await path(lang);
+          const result = await path(fileSuffix);
           if (result) {
             setMdData(result);
           }
@@ -30,7 +35,7 @@ const useMdData = (path?: (lang?: string) => Promise<CodeBlockData>, name: strin
     };
     getMd();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [lang]);
+  }, [fileSuffix]);
   return { mdData, loading };
 };
 
