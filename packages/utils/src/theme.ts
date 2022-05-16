97bbc9b24496c6533a8c188ecb3970b02d8365d9
SunLxy <1011771396@qq.com>
2022-05-16 09:00:53 +0800

    feat:新增css变量转换主题变量方法规则

---
+++ packages/utils/src/theme.ts
@@ -0,0 +1,28 @@
+export type CssVariableType = Record<string, string | number>;
+
+/**
+ * @description: 转换驼峰命名
+ * @param {string} value 变量值
+ * @param {string} ident 前缀标识
+ */
+export const transformationHump = (value: string, ident: string) => {
+  const Reg = new RegExp(`^${ident}`);
+  return value.replace(Reg, '').replace(/\-(\w)/g, function (all, letter) {
+    return letter.toUpperCase();
+  });
+};
+
+/**
+ * `css 变量`转换成`主题变量`规则 ：css变量去除标识(`--w-`)之后，进行转换 驼峰命名
+ * @description: css 变量转换主题变量
+ * @param {CssVariableType} cssVariable css变量集合
+ * @param {string} ident 前缀标识
+ */
+export const transformationVariable = (cssVariable: CssVariableType, ident: string) => {
+  const Variable: Record<string, string> = {};
+  Object.keys(cssVariable || {}).forEach((key) => {
+    const name = transformationHump(key, ident);
+    Variable[name] = `var(${key})`;
+  });
+  return Variable;
+};
