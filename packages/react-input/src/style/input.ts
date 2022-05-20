c5b6e929f15842bcc0f3c528ea5e96af9fd55510
WmW <zhouzheng0646@qq.com>
2022-05-20 18:29:22 +0800

    refactor(Input): 拆分属性 #826 (#844)

--- packages/react-input/src/style/input.ts
+++ packages/react-input/src/style/input.ts
@@ -4,66 +4,84 @@ import { InputProps } from 'src';
 
 interface InputWarp extends ThemeVariantValueOptions, Pick<InputProps, 'size' | 'addonAfter' | 'disabled'> {}
 
-export const Input = styled.input`
-  outline: none;
-  border: none;
-  border-radius: 3px;
-  box-shadow: 0 0 0 0 rgba(19, 124, 189, 0), 0 0 0 0 rgba(19, 124, 189, 0), inset 0 0 0 1px rgba(16, 22, 26, 0.15),
-    inset 0 1px 1px rgba(16, 22, 26, 0.2);
-  box-sizing: border-box;
-  background: #fff;
-  height: 30px;
-  margin: 0 !important;
-  padding: 0 10px;
-  vertical-align: middle;
-  line-height: 30px;
-  color: #393e48;
-  font-weight: 400;
-  font-size: inherit;
-  transition: box-shadow 0.3s cubic-bezier(0.4, 1, 0.75, 0.9);
-  appearance: none;
-  &:not(:first-child) {
-    padding-left: 26px;
-  }
-  &:focus {
-    box-shadow: 0 0 0 1px #393e48, 0 0 0 3px rgba(57, 62, 72, 0.17), inset 0 1px 1px rgba(16, 22, 26, 0.2);
-  }
-  &:hover {
-    box-shadow: 0 0 0 1px #6e6e6e, 0 0 0 3px rgba(57, 62, 72, 0), inset 0 1px 1px rgba(16, 22, 26, 0.2);
-  }
-  &:focus&:hover {
-    box-shadow: 0 0 0 1px #6e6e6e, 0 0 0 3px rgba(57, 62, 72, 0.17), inset 0 1px 1px rgba(16, 22, 26, 0.2);
-  }
-  &:disabled {
-    box-shadow: none;
-    background: #dddddd;
-    opacity: 0.75;
-    color: #a5a5a5;
-    cursor: not-allowed;
-    resize: none;
-  }
+export const Input = styled.input<InputWarp>`
+  ${(props) => {
+    const boxShadowColorInputBase = getThemeVariantValue(props, `boxShadowColorInputBase`);
+    const boxShadowColorInputInsHBase = getThemeVariantValue(props, `boxShadowColorInputInsHBase`);
+    const boxShadowColorInputInsVBase = getThemeVariantValue(props, `boxShadowColorInputInsVBase`);
+    const boxShadowColorInputVFocus = getThemeVariantValue(props, `boxShadowColorInputVFocus`);
+    const boxShadowColorInputHHover = getThemeVariantValue(props, `boxShadowColorInputHHover`);
+
+    return css`
+      outline: none;
+      border: none;
+      border-radius: 3px;
+      box-shadow: 0 0 0 0 ${boxShadowColorInputBase}, 0 0 0 0 ${boxShadowColorInputBase},
+        inset 0 0 0 1px ${boxShadowColorInputInsHBase}, inset 0 1px 1px ${boxShadowColorInputInsVBase};
+      box-sizing: border-box;
+      background: ${getThemeVariantValue(props, `backgroundColorInputBase`)};
+      height: ${getThemeVariantValue(props, `heightInputDefault`)};
+      margin: 0 !important;
+      padding: 0 10px;
+      vertical-align: middle;
+      line-height: ${getThemeVariantValue(props, `heightInputDefault`)};
+      color: ${getThemeVariantValue(props, `colorInputBase`)};
+      font-weight: 400;
+      font-size: ${getThemeVariantValue(props, `fontSizeInputDefault`)};
+      transition: box-shadow 0.3s cubic-bezier(0.4, 1, 0.75, 0.9);
+      appearance: none;
+      &:not(:first-child) {
+        padding-left: 26px;
+      }
+      &:focus {
+        box-shadow: 0 0 0 1px ${getThemeVariantValue(props, `boxShadowColorInputHFocus`)},
+          0 0 0 3px ${boxShadowColorInputVFocus}, inset 0 1px 1px ${boxShadowColorInputInsVBase};
+      }
+      &:hover {
+        box-shadow: 0 0 0 1px ${boxShadowColorInputHHover},
+          0 0 0 3px ${getThemeVariantValue(props, `boxShadowColorInputVHover`)},
+          inset 0 1px 1px ${boxShadowColorInputInsVBase};
+      }
+      &:focus&:hover {
+        box-shadow: 0 0 0 1px ${boxShadowColorInputHHover}, 0 0 0 3px ${boxShadowColorInputVFocus},
+          inset 0 1px 1px ${boxShadowColorInputInsVBase};
+      }
+      &:disabled {
+        box-shadow: none;
+        background: ${getThemeVariantValue(props, `backgrounColorInputDisabled`)};
+        opacity: 0.75;
+        color: ${getThemeVariantValue(props, `colorInputDisabled`)};
+        cursor: not-allowed;
+        resize: none;
+      }
+    `;
+  }}
 `;
 
-export const InputAddonAfter = styled.span`
-  position: absolute;
-  top: 3px;
-  right: 3px;
-  display: flex;
-  bottom: 3px;
-  > * {
-    display: flex !important;
-    align-items: center;
-  }
+export const InputAddonAfter = styled.span<InputWarp>`
+  ${(props) => {
+    return css`
+      position: absolute;
+      top: ${getThemeVariantValue(props, `topInputAddonAfter`)};
+      right: ${getThemeVariantValue(props, `topInputAddonAfter`)};
+      display: flex;
+      bottom: ${getThemeVariantValue(props, `topInputAddonAfter`)};
+      > * {
+        display: flex !important;
+        align-items: center;
+      }
+    `;
+  }}
 `;
 
 const InputWarp = styled.div<InputWarp>`
   position: relative;
   font-size: ${(props) => getThemeVariantValue(props, 'fontSizeDefault')};
-  line-height: '14px';
-  width: 100%;
+  line-height: ${(props) => getThemeVariantValue(props, 'lineHeightInputsDefault')};
+  width: ${(props) => getThemeVariantValue(props, 'widthInputsDefault')};
 
   & ${Input} {
-    width: 100%;
+    width: ${(props) => getThemeVariantValue(props, 'widthInputsDefault')};
   }
 
   > .w-icon {
@@ -76,10 +94,10 @@ const InputWarp = styled.div<InputWarp>`
   ${(props) => {
     if (props.size === 'large') {
       return css`
-        font-size: ${(props) => getThemeVariantValue(props, 'fontSizeLarge')};
+        font-size: ${getThemeVariantValue(props, 'fontSizeLarge')};
         ${Input} {
-          line-height: 36px;
-          height: 36px;
+          line-height: ${getThemeVariantValue(props, 'lineHeightInputLarge')};
+          height: ${getThemeVariantValue(props, 'lineHeightInputLarge')};
         }
       `;
     }
@@ -90,8 +108,8 @@ const InputWarp = styled.div<InputWarp>`
       return css`
         min-width: 20px;
         ${Input} {
-          line-height: 24px;
-          height: 24px;
+          line-height: ${getThemeVariantValue(props, 'lineHeightInputSmall')};
+          height: ${getThemeVariantValue(props, 'lineHeightInputSmall')};
           padding: 0 6px;
           &:not(:first-child) {
             padding-left: 26px;
@@ -99,8 +117,8 @@ const InputWarp = styled.div<InputWarp>`
         }
         ${InputAddonAfter} {
           > * {
-            line-height: 16px;
-            min-height: 16px;
+            line-height: ${getThemeVariantValue(props, 'lineHeightInputAddonAfter')};
+            min-height: ${getThemeVariantValue(props, 'lineHeightInputAddonAfter')};
           }
         }
       `;
@@ -108,11 +126,39 @@ const InputWarp = styled.div<InputWarp>`
   }}
 `;
 
+Input.defaultProps = {
+  defaultTheme: {
+    colorInputBase: '#393e48',
+    boxShadowColorInputHBase: 'rgba(19, 124, 189, 0)',
+    boxShadowColorInputInsHBase: 'rgba(16, 22, 26, 0.15)',
+    boxShadowColorInputInsVBase: 'rgba(16, 22, 26, 0.2)',
+    backgroundColorInputBase: '#fff',
+    heightInputDefault: '30px',
+    fontSizeInputDefault: 'inherit',
+    boxShadowColorInputHFocus: '#393e48',
+    boxShadowColorInputVFocus: 'rgba(57, 62, 72, 0.17)',
+    boxShadowColorInputHHover: '#6e6e6e',
+    boxShadowColorInputVHover: 'rgba(57, 62, 72, 0)',
+    backgrounColorInputDisabled: '#dddddd',
+    colorInputDisabled: '#a5a5a5',
+  },
+};
+
+InputAddonAfter.defaultProps = {
+  defaultTheme: {
+    topInputAddonAfter: '3px',
+  },
+};
+
 InputWarp.defaultProps = {
   defaultTheme: {
-    fontSizeSmall: '12px',
     fontSizeDefault: '14px',
     fontSizeLarge: '16px',
+    lineHeightInputsDefault: '14px',
+    widthInputsDefault: '100%',
+    lineHeightInputLarge: '36px',
+    lineHeightInputSmall: '24px',
+    lineHeightInputAddonAfter: '16px',
   },
 };
 
