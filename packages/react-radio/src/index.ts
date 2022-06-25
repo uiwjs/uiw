6f1d6103430b433f2c1a7149cea448ccfb288caf
姚宾 <yaob421123@163.com>
2022-06-25 17:38:51 +0800

    refactor(checkbox):重构多选框

--- packages/react-radio/src/index.ts
+++ packages/react-radio/src/index.ts
@@ -2,6 +2,7 @@ export * from './Radio';
 export * from './RadioGroup';
 export * from './RadioAbstract';
 export * from './RadioButton';
+export * from './style';
 export { default as Radio } from './Radio';
 export { default as RadioGroup } from './RadioGroup';
 export { default as RadioButton } from './RadioButton';
