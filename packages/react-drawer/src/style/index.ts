8d097345f63b3b60cefb8b38c3c9ff8c54d8ffd3
zhouzheng <zhouzheng0646@qq.com>
2022-07-31 15:58:12 +0800

    refactor(Drawer): 重构Drawer 抽屉组件

--- packages/react-drawer/src/style/index.ts
+++ packages/react-drawer/src/style/index.ts
@@ -1,11 +1,12 @@
 import styled, { css } from 'styled-components';
 import { ThemeVariantValueOptions, getThemeVariantValue } from '@uiw/utils';
-import Overlay from '@uiw/react-overlay';
+import Overlay, { ContentWrap } from '@uiw/react-overlay';
 import { DrawerProps } from 'src';
 
 interface DrawerWrapperWrapProps extends ThemeVariantValueOptions {}
 interface DrawerHeaderWrapProps extends ThemeVariantValueOptions {}
 interface DrawerHeaderWrapProps extends ThemeVariantValueOptions {}
+interface DrawerWrapProps extends Pick<DrawerProps, 'placement'> {}
 export const DrawerWrapperWrap = styled.div<DrawerWrapperWrapProps>`
   ${(props) => css`
     box-shadow: 0 0 0 1px ${getThemeVariantValue(props, 'boxShadowColorInDrawerWrapper')},
@@ -17,13 +18,6 @@ export const DrawerWrapperWrap = styled.div<DrawerWrapperWrapProps>`
     flex-direction: column;
   `}
 `;
-DrawerWrapperWrap.defaultProps = {
-  defaultTheme: {
-    boxShadowColorInDrawerWrapper: 'rgba(16, 22, 26, 0.1)',
-    boxShadowColorHvDrawerWrapper: 'rgba(16, 22, 26, 0.2)',
-    backgroundColorDrawerWrapper: '#fff',
-  },
-};
 
 export const DrawerHeaderWrap = styled.div<DrawerHeaderWrapProps>`
   ${(props) => css`
@@ -47,13 +41,6 @@ export const DrawerHeaderWrap = styled.div<DrawerHeaderWrapProps>`
     }
   `}
 `;
-DrawerHeaderWrap.defaultProps = {
-  defaultTheme: {
-    backgroundColorDrawerHeader: '#fff',
-    boxShadowColorInDrawerHeader: 'rgba(16, 22, 26, 0.15)',
-    colorHvDrawerHeader: '#393e48',
-  },
-};
 
 export const DrawerBodyWrap = styled.div`
   ${(props) => css`
@@ -73,19 +60,12 @@ export const DrawerFooterWrap = styled.div<DrawerHeaderWrapProps>`
     align-items: center;
   `}
 `;
-DrawerFooterWrap.defaultProps = {
-  defaultTheme: {
-    boxShadowColorInDrawerFooter: 'rgba(16, 22, 26, 0.15)',
-  },
-};
 
 export const DrawerBodyClsWrap = styled.div`
   ${(props) => css`
     padding: 15px;
   `}
 `;
-
-interface DrawerWrapProps extends Pick<DrawerProps, 'placement'> {}
 
 export const DrawerWrap = styled(Overlay)<DrawerWrapProps>`
   ${(props) => css`
@@ -131,82 +111,104 @@ export const DrawerWrap = styled(Overlay)<DrawerWrapProps>`
       ${DrawerWrapperWrap} {
         left: 0;
       }
-    `} /* &.w-overlay-enter ${ContentWrap} {
-    opacity: 1;
-  }
-  &.w-overlay-enter-active ${ContentWrap} {
-    transition: all 0.2s cubic-bezier(0.4, 1, 0.75, 0.9);
-  }
-  &.w-overlay-exit ${ContentWrap} {
-    opacity: 1;
-  }
-  &.w-overlay-exit-active ${ContentWrap} {
-    transition: all 0.2s cubic-bezier(0.4, 1, 0.75, 0.9);
-  }
-  &.right.w-overlay-enter ${ContentWrap} {
-    background: red;
-    -webkit-transform: translateX(100%);
-            transform: translateX(100%);
-  }
-  &.right.w-overlay-enter-active ${ContentWrap} {
-    -webkit-transform: translateX(0);
-            transform: translateX(0);
-  }
-  &.right.w-overlay-exit ${ContentWrap} {
-    -webkit-transform: translateX(0);
-            transform: translateX(0);
-  }
-  &.right.w-overlay-exit-active ${ContentWrap} {
-    -webkit-transform: translateX(100%);
-            transform: translateX(100%);
-  }
-  &.left.w-overlay-enter ${ContentWrap} {
-    -webkit-transform: translateX(-100%);
-            transform: translateX(-100%);
-  }
-  &.left.w-overlay-enter-active ${ContentWrap} {
-    -webkit-transform: translateX(0);
-            transform: translateX(0);
-  }
-  &.left.w-overlay-exit ${ContentWrap} {
-    -webkit-transform: translateX(0);
-            transform: translateX(0);
-  }
-  &.left.w-overlay-exit-active ${ContentWrap} {
-    -webkit-transform: translateX(-100%);
-            transform: translateX(-100%);
-  }
-  &.top.w-overlay-enter ${ContentWrap} {
-    -webkit-transform: translateY(-100%);
-            transform: translateY(-100%);
-  }
-  &.top.w-overlay-enter-active ${ContentWrap} {
-    -webkit-transform: translateY(0);
-            transform: translateY(0);
-  }
-  &.top.w-overlay-exit ${ContentWrap} {
-    -webkit-transform: translateY(0);
-            transform: translateY(0);
-  }
-  &.top.w-overlay-exit-active ${ContentWrap} {
-    -webkit-transform: translateY(-100%);
-            transform: translateY(-100%);
-  }
-  &.bottom.w-overlay-enter ${ContentWrap} {
-    -webkit-transform: translateY(100%);
-            transform: translateY(100%);
-  }
-  &.bottom.w-overlay-enter-active ${ContentWrap} {
-    -webkit-transform: translateY(0);
-            transform: translateY(0);
-  }
-  &.bottom.w-overlay-exit ${ContentWrap} {
-    -webkit-transform: translateY(0);
-            transform: translateY(0);
-  }
-  &.bottom.w-overlay-exit-active ${ContentWrap} {
-    -webkit-transform: translateY(100%);
-            transform: translateY(100%);
-  } */
+    `}
+
+
+  &.w-overlay-enter ${ContentWrap} {
+      opacity: 1;
+    }
+    &.w-overlay-enter-active ${ContentWrap} {
+      transition: all 0.2s cubic-bezier(0.4, 1, 0.75, 0.9);
+    }
+    &.w-overlay-exit ${ContentWrap} {
+      opacity: 1;
+    }
+    &.w-overlay-exit-active ${ContentWrap} {
+      transition: all 0.2s cubic-bezier(0.4, 1, 0.75, 0.9);
+    }
+
+    ${props.placement === 'right' &&
+    css`
+      &.w-overlay-enter ${ContentWrap} {
+        transform: translateX(100%);
+      }
+      &.w-overlay-enter-active ${ContentWrap} {
+        transform: translateX(0);
+      }
+      &.w-overlay-exit ${ContentWrap} {
+        transform: translateX(0);
+      }
+      &.w-overlay-exit-active ${ContentWrap} {
+        transform: translateX(100%);
+      }
+    `}
+
+    ${props.placement === 'left' &&
+    css`
+      &.w-overlay-enter ${ContentWrap} {
+        transform: translateX(-100%);
+      }
+      &.w-overlay-enter-active ${ContentWrap} {
+        transform: translateX(0);
+      }
+      &.w-overlay-exit ${ContentWrap} {
+        transform: translateX(0);
+      }
+      &.w-overlay-exit-active ${ContentWrap} {
+        transform: translateX(-100%);
+      }
+    `}
+
+  ${props.placement === 'top' &&
+    css`
+      &.w-overlay-enter ${ContentWrap} {
+        transform: translateY(-100%);
+      }
+      &.w-overlay-enter-active ${ContentWrap} {
+        transform: translateY(0);
+      }
+      &.w-overlay-exit ${ContentWrap} {
+        transform: translateY(0);
+      }
+      &.w-overlay-exit-active ${ContentWrap} {
+        transform: translateY(-100%);
+      }
+    `}
+
+  ${props.placement === 'bottom' &&
+    css`
+      &.w-overlay-enter ${ContentWrap} {
+        transform: translateY(100%);
+      }
+      &.w-overlay-enter-active ${ContentWrap} {
+        transform: translateY(0);
+      }
+      &.w-overlay-exit ${ContentWrap} {
+        transform: translateY(0);
+      }
+      &.w-overlay-exit-active ${ContentWrap} {
+        transform: translateY(100%);
+      }
+    `}
   `}
 `;
+
+DrawerWrapperWrap.defaultProps = {
+  defaultTheme: {
+    boxShadowColorInDrawerWrapper: 'rgba(16, 22, 26, 0.1)',
+    boxShadowColorHvDrawerWrapper: 'rgba(16, 22, 26, 0.2)',
+    backgroundColorDrawerWrapper: '#fff',
+  },
+};
+DrawerHeaderWrap.defaultProps = {
+  defaultTheme: {
+    backgroundColorDrawerHeader: '#fff',
+    boxShadowColorInDrawerHeader: 'rgba(16, 22, 26, 0.15)',
+    colorHvDrawerHeader: '#393e48',
+  },
+};
+DrawerFooterWrap.defaultProps = {
+  defaultTheme: {
+    boxShadowColorInDrawerFooter: 'rgba(16, 22, 26, 0.15)',
+  },
+};
